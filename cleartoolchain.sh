148ae8c17b5e863ee31e1ffc8d62f5a2e186bc3b
Chase Bradley <bradleycha@proton.me>
2025-01-23 21:28:37 -0500

    Create cleartoolchain script

---
+++ cleartoolchain.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+# ----------------------------------------------------------------------------
+#                          Copyright (c) Tux64 2025
+# ----------------------------------------------------------------------------
+# cleartoolchain.sh - Clears the toolchain environment variables set by
+#    usetoolchain.sh.
+# ----------------------------------------------------------------------------
+
+CC=
+CXX=
+AR=
+AS=
+DSYMUTIL=
+LD=
+NM=
+RANLIB=
+STRIP=
+OBJCOPY=
+OBJDUMP=
+OTOOL=
+READELF=
+CC_FOR_TARGET=
+CXX_FOR_TARGET=
+AR_FOR_TARGET=
+AS_FOR_TARGET=
+DSYMUTIL_FOR_TARGET=
+LD_FOR_TARGET=
+NM_FOR_TARGET=
+RANLIB_FOR_TARGET=
+STRIP_FOR_TARGET=
+OBJCOPY_FOR_TARGET=
+OBJDUMP_FOR_TARGET=
+OTOOL_FOR_TARGET=
+READELF_FOR_TARGET=
+
