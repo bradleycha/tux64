f99cc4093e0c6533e159d3dadcf03262cc2df317
Chase Bradley <bradleycha@proton.me>
2025-05-29 21:11:39 -0400

    Create tux64-develop headers, structs, and linker scripts to access MMIO as global variables in C

--- develop/headers/platform_n64/ai.h
+++ develop/headers/platform_n64/ai.h
@@ -15,33 +15,33 @@
 /* ...hey look guys, we have AI on the N64!  now please fund my AI startup */
 /* which specializes in real-time AI-generation of sound! ho, ho, ho! */
 
-/* addresses of AI MMIO registers */
-#define TUX64_PLATFORM_N64_AI_MMIO_DRAM_ADDR\
+/* addresses of AI registers */
+#define TUX64_PLATFORM_N64_AI_ADDRESS_DRAM_ADDR\
    (\
       TUX64_PLATFORM_N64_MEMORY_MAP_ADDRESS_AI +\
       0x00000000u\
    )
-#define TUX64_PLATFORM_N64_AI_MMIO_LENGTH\
+#define TUX64_PLATFORM_N64_AI_ADDRESS_LENGTH\
    (\
       TUX64_PLATFORM_N64_MEMORY_MAP_ADDRESS_AI +\
       0x00000004u\
    )
-#define TUX64_PLATFORM_N64_AI_MMIO_CONTROL\
+#define TUX64_PLATFORM_N64_AI_ADDRESS_CONTROL\
    (\
       TUX64_PLATFORM_N64_MEMORY_MAP_ADDRESS_AI +\
       0x00000008u\
    )
-#define TUX64_PLATFORM_N64_AI_MMIO_STATUS\
+#define TUX64_PLATFORM_N64_AI_ADDRESS_STATUS\
    (\
       TUX64_PLATFORM_N64_MEMORY_MAP_ADDRESS_AI +\
       0x0000000cu\
    )
-#define TUX64_PLATFORM_N64_AI_MMIO_DACRATE\
+#define TUX64_PLATFORM_N64_AI_ADDRESS_DAC_RATE\
    (\
       TUX64_PLATFORM_N64_MEMORY_MAP_ADDRESS_AI +\
       0x00000010u\
    )
-#define TUX64_PLATFORM_N64_AI_MMIO_BITRATE\
+#define TUX64_PLATFORM_N64_AI_ADDRESS_BIT_RATE\
    (\
       TUX64_PLATFORM_N64_MEMORY_MAP_ADDRESS_AI +\
       0x00000014u\
---
+++ develop/headers/platform_n64/mmio.h
@@ -0,0 +1,135 @@
+/*----------------------------------------------------------------------------*/
+/*                          Copyright (C) Tux64 2025                          */
+/*                    https://github.com/bradleycha/tux64                     */
+/*----------------------------------------------------------------------------*/
+/* develop/headers/platform_n64/mmio.h - Memory-mapped I/O (MMIO).            */
+/*----------------------------------------------------------------------------*/
+
+#ifndef _TUX64_PLATFORM_N64_MMIO_H
+#define _TUX64_PLATFORM_N64_MMIO_H
+/*------------------------------------------------------------------------------*/
+
+#include "tux64/tux64.h"
+
+struct Tux64PlatformN64MmioRegistersMi {
+   Tux64UInt32       mode;
+   const Tux64UInt32 version;
+   const Tux64UInt32 interrupt;
+   Tux64UInt32       mask;
+   Tux64UInt8        __pad_000c [0x04];
+   Tux64UInt32       ique_secure_exception;
+   Tux64UInt8        __pad_0018 [0x14];
+   const Tux64UInt32 ique_random;
+   Tux64UInt8        __pad_0030 [0x08];
+   Tux64UInt32       ique_interrupt;
+   Tux64UInt32       ique_mask;
+};
+
+struct Tux64PlatformN64MmioRegistersVi {
+   Tux64UInt32 ctrl;
+   Tux64UInt32 origin;
+   Tux64UInt32 width;
+   Tux64UInt32 v_intr;
+   Tux64UInt32 v_current;
+   Tux64UInt32 burst;
+   Tux64UInt32 v_total;
+   Tux64UInt32 h_total;
+   Tux64UInt32 h_total_leap;
+   Tux64UInt32 h_video;
+   Tux64UInt32 v_video;
+   Tux64UInt32 v_burst;
+   Tux64UInt32 x_scale;
+   Tux64UInt32 y_scale;
+   Tux64UInt32 test_addr;
+   Tux64UInt32 staged_data;
+};
+
+struct Tux64PlatformN64MmioRegistersAi {
+   Tux64UInt32       dram_addr;
+   Tux64UInt32       length;
+   Tux64UInt32       control;
+   const Tux64UInt32 status;
+   Tux64UInt32       dac_rate;
+   Tux64UInt32       bit_rate;
+};
+
+#define TUX64_PLATFORM_N64_MMIO_REGISTERS_PI_BSD_DOM_COUNT\
+   (2u)
+#define TUX64_PLATFORM_N64_MMIO_REGISTERS_PI_IQUE_ATB_LOWER_COUNT\
+   (192u)
+
+struct Tux64PlatformN64MmioRegistersPiBsdDom {
+   Tux64UInt32 lat;
+   Tux64UInt32 pwd;
+   Tux64UInt32 pgs;
+   Tux64UInt32 rls;
+};
+
+struct Tux64PlatformN64MmioRegistersPi {
+   Tux64UInt32 dram_addr;
+   Tux64UInt32 cart_addr;
+   Tux64UInt32 rd_len;
+   Tux64UInt32 wr_len;
+   Tux64UInt32 status;
+   struct Tux64PlatformN64MmioRegistersPiBsdDom
+   bsd_dom [TUX64_PLATFORM_N64_MMIO_REGISTERS_PI_BSD_DOM_COUNT];
+   Tux64UInt8 __pad_0030 [0x0c];
+   Tux64UInt32 ique_atb_upper;
+   Tux64UInt8 __pad_0034 [0x04];
+   Tux64UInt32 ique_nand_ctrl;
+   Tux64UInt32 ique_nand_cfg;
+   Tux64UInt8 __pad_0050 [0x08];
+   Tux64UInt32 ique_rd_len;
+   Tux64UInt32 ique_wr_len;
+   Tux64UInt32 ique_gpio;
+   Tux64UInt8 __pad_0064 [0x0c];
+   Tux64UInt32 ique_nand_addr;
+   Tux64UInt8 __pad_0074 [0x0001048c];
+   Tux64UInt32 ique_atb_lower [TUX64_PLATFORM_N64_MMIO_REGISTERS_PI_IQUE_ATB_LOWER_COUNT];
+};
+
+struct Tux64PlatformN64MmioRegistersRi {
+   Tux64UInt32       mode;
+   Tux64UInt32       config;
+   Tux64UInt32       current_load;
+   Tux64UInt32       select;
+   Tux64UInt32       refresh;
+   Tux64UInt32       latency;
+   const Tux64UInt32 error;
+   const Tux64UInt32 bank_status;
+};
+
+struct Tux64PlatformN64MmioRegistersSi {
+   Tux64UInt32       dram_addr;
+   Tux64UInt32       pif_ad_rd64b;
+   Tux64UInt32       pif_ad_wr4b;
+   Tux64UInt8        __pad_000c [0x04];
+   Tux64UInt32       pif_ad_wr64b;
+   Tux64UInt32       pif_ad_rd4b;
+   const Tux64UInt32 status;
+};
+
+/* Memory-mapped I/O registers which can be directly accessed.  These will be */
+/* defined and relocated to the correct memory address by the linker. */
+extern volatile struct Tux64PlatformN64MmioRegistersMi
+tux64_platform_n64_mmio_registers_mi
+__attribute__((section(".tux64.platform_n64.mmio.mi")));
+extern volatile struct Tux64PlatformN64MmioRegistersVi
+tux64_platform_n64_mmio_registers_vi
+__attribute__((section(".tux64.platform_n64.mmio.vi")));
+extern volatile struct Tux64PlatformN64MmioRegistersAi
+tux64_platform_n64_mmio_registers_ai
+__attribute__((section(".tux64.platform_n64.mmio.ai")));
+extern volatile struct Tux64PlatformN64MmioRegistersPi
+tux64_platform_n64_mmio_registers_pi
+__attribute__((section(".tux64.platform_n64.mmio.pi")));
+extern volatile struct Tux64PlatformN64MmioRegistersRi
+tux64_platform_n64_mmio_registers_ri
+__attribute__((section(".tux64.platform_n64.mmio.ri")));
+extern volatile struct Tux64PlatformN64MmioRegistersSi
+tux64_platform_n64_mmio_registers_si
+__attribute__((section(".tux64.platform_n64.mmio.si")));
+
+/*------------------------------------------------------------------------------*/
+#endif /* _TUX64_PLATFORM_N64_MMIO_H */
+
