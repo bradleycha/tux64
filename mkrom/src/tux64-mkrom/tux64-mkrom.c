41000672870fb8f3873412622e0d40891f30d375
Chase Bradley <bradleycha@proton.me>
2025-06-03 23:26:52 -0400

    tux64-mkrom: set up basic autotools project stuff

---
+++ mkrom/src/tux64-mkrom/tux64-mkrom.c
@@ -0,0 +1,17 @@
+/*----------------------------------------------------------------------------*/
+/*                          Copyright (C) Tux64 2025                          */
+/*                    https://github.com/bradleycha/tux64                     */
+/*----------------------------------------------------------------------------*/
+/* mkrom/src/tux64-mkrom/tux64-mkrom.c - Main application entrypoint for      */
+/*    tux64-mkrom.                                                            */
+/*----------------------------------------------------------------------------*/
+
+#include "tux64-mkrom/tux64-mkrom.h"
+
+#include <stdio.h>
+
+int main(void) {
+   (void)fputs("Hello from tux64-mkrom!\n", stdout);
+   return 0;
+}
+
