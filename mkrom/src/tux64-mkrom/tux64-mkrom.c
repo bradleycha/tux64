59d3467173ab1d9e06a84509060bebfd73a38ca1
Chase Bradley <bradleycha@proton.me>
2025-06-05 14:19:50 -0400

    tux64-mkrom: add checked downcast and printing of exit status to stderr

--- mkrom/src/tux64-mkrom/tux64-mkrom.c
+++ mkrom/src/tux64-mkrom/tux64-mkrom.c
@@ -11,28 +11,74 @@
 #include <stdio.h>
 
 enum Tux64MkromExitStatus {
-   TUX64_MKROM_EXIT_STATUS_OK
+   TUX64_MKROM_EXIT_STATUS_OK,
+   TUX64_MKROM_EXIT_STATUS_TOO_MANY_ARGUMENTS
 };
 
-static enum Tux64MkromExitStatus
+
+union Tux64MkromExitPayload {
+   Tux64UInt8 temp;
+};
+
+struct Tux64MkromExitResult {
+   enum Tux64MkromExitStatus status;
+   union Tux64MkromExitPayload payload;
+};
+
+static void
+tux64_mkrom_log_error(
+   const char * message
+) {
+   (void)fprintf(stderr, "error: %s\n", message);
+   return;
+}
+
+static void
+tux64_mkrom_exit_result_display(
+   const struct Tux64MkromExitResult * self
+) {
+   switch (self->status) {
+      case TUX64_MKROM_EXIT_STATUS_OK:
+         break;
+      case TUX64_MKROM_EXIT_STATUS_TOO_MANY_ARGUMENTS:
+         tux64_mkrom_log_error("too many arguments");
+         break;
+   }
+
+   return;
+}
+
+static struct Tux64MkromExitResult
 tux64_mkrom_main(
    const char * const * args,
-   Tux64UInt32 args_count
+   Tux64UInt8 args_count
 ) {
+   struct Tux64MkromExitResult result;
+
+   /* TODO: implement */
    (void)args;
    (void)args_count;
    (void)fputs("Hello from tux64-mkrom!\n", stdout);
-   return TUX64_MKROM_EXIT_STATUS_OK;
+
+   result.status = TUX64_MKROM_EXIT_STATUS_OK;
+   return result;
 }
 
 int main(int argc, char ** argv) {
-   enum Tux64MkromExitStatus exit_status;
+   struct Tux64MkromExitResult exit_result;
 
-   exit_status = tux64_mkrom_main(
+   if (argc > TUX64_UINT8_MAX) {
+      exit_result.status = TUX64_MKROM_EXIT_STATUS_TOO_MANY_ARGUMENTS;
+      goto exit;
+   }
+
+   exit_result = tux64_mkrom_main(
       (const char * const *)argv,
-      (Tux64UInt32)argc
+      (Tux64UInt8)argc
    );
 
-   return (int)exit_status;
+exit:
+   tux64_mkrom_exit_result_display(&exit_result);
+   return (int)exit_result.status;
 }
 
