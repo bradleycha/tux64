8d610f7e711954242b9ff3fa224079ee7e12002f
Chase Bradley <bradleycha@proton.me>
2025-06-04 20:28:58 -0400

    tux64-mkrom: create better entrypoint

--- mkrom/src/tux64-mkrom/tux64-mkrom.c
+++ mkrom/src/tux64-mkrom/tux64-mkrom.c
@@ -10,8 +10,29 @@
 
 #include <stdio.h>
 
-int main(void) {
+enum Tux64MkromExitStatus {
+   TUX64_MKROM_EXIT_STATUS_OK
+};
+
+static enum Tux64MkromExitStatus
+tux64_mkrom_main(
+   const char * const * args,
+   Tux64UInt32 args_count
+) {
+   (void)args;
+   (void)args_count;
    (void)fputs("Hello from tux64-mkrom!\n", stdout);
-   return 0;
+   return TUX64_MKROM_EXIT_STATUS_OK;
+}
+
+int main(int argc, char ** argv) {
+   enum Tux64MkromExitStatus exit_status;
+
+   exit_status = tux64_mkrom_main(
+      (const char * const *)argv,
+      (Tux64UInt32)argc
+   );
+
+   return (int)exit_status;
 }
 
