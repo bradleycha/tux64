7fbc949b95cc06589ec9fb35963c08b91cc659c0
Chase Bradley <bradleycha@proton.me>
2025-06-05 22:18:51 -0400

    tux64-mkrom: add argp.h for arguments parsing

--- mkrom/src/tux64-mkrom/tux64-mkrom.c
+++ mkrom/src/tux64-mkrom/tux64-mkrom.c
@@ -9,15 +9,19 @@
 #include "tux64-mkrom/tux64-mkrom.h"
 
 #include <stdio.h>
+#include <argp.h>
 
 enum Tux64MkromExitStatus {
    TUX64_MKROM_EXIT_STATUS_OK,
-   TUX64_MKROM_EXIT_STATUS_TOO_MANY_ARGUMENTS
+   TUX64_MKROM_EXIT_STATUS_INVALID_ARGUMENTS
 };
 
+struct Tux64MkromExitPayloadInvalidArguments {
+   error_t reason;
+};
 
 union Tux64MkromExitPayload {
-   Tux64UInt8 temp;
+   struct Tux64MkromExitPayloadInvalidArguments invalid_arguments;
 };
 
 struct Tux64MkromExitResult {
@@ -26,10 +30,11 @@ struct Tux64MkromExitResult {
 };
 
 static void
-tux64_mkrom_log_error(
-   const char * message
+tux64_mkrom_exit_result_display_invalid_arguments(
+   const struct Tux64MkromExitPayloadInvalidArguments * self
 ) {
-   (void)fprintf(stderr, "error: %s\n", message);
+   /* TODO: implement */
+   (void)self;
    return;
 }
 
@@ -40,24 +45,23 @@ tux64_mkrom_exit_result_display(
    switch (self->status) {
       case TUX64_MKROM_EXIT_STATUS_OK:
          break;
-      case TUX64_MKROM_EXIT_STATUS_TOO_MANY_ARGUMENTS:
-         tux64_mkrom_log_error("too many arguments");
+      case TUX64_MKROM_EXIT_STATUS_INVALID_ARGUMENTS:
+         tux64_mkrom_exit_result_display_invalid_arguments(&self->payload.invalid_arguments);
          break;
+      default:
+         TUX64_UNREACHABLE;
    }
 
    return;
 }
 
 static struct Tux64MkromExitResult
-tux64_mkrom_main(
-   const char * const * args,
-   Tux64UInt8 args_count
-) {
+tux64_mkrom_main(int argc, char ** argv) {
    struct Tux64MkromExitResult result;
 
    /* TODO: implement */
-   (void)args;
-   (void)args_count;
+   (void)argc;
+   (void)argv;
    (void)fputs("Hello from tux64-mkrom!\n", stdout);
 
    result.status = TUX64_MKROM_EXIT_STATUS_OK;
@@ -67,17 +71,8 @@ tux64_mkrom_main(
 int main(int argc, char ** argv) {
    struct Tux64MkromExitResult exit_result;
 
-   if (argc > TUX64_UINT8_MAX) {
-      exit_result.status = TUX64_MKROM_EXIT_STATUS_TOO_MANY_ARGUMENTS;
-      goto exit;
-   }
+   exit_result = tux64_mkrom_main(argc, argv);
 
-   exit_result = tux64_mkrom_main(
-      (const char * const *)argv,
-      (Tux64UInt8)argc
-   );
-
-exit:
    tux64_mkrom_exit_result_display(&exit_result);
    return (int)exit_result.status;
 }
