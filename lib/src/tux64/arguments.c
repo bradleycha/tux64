2fa511290fd78dd686e57c7137f6636cca794b67
Chase Bradley <bradleycha@proton.me>
2025-06-09 14:08:23 -0400

    tux64-lib: add checking of required arguments to argument parser

--- lib/src/tux64/arguments.c
+++ lib/src/tux64/arguments.c
@@ -10,6 +10,7 @@
 #include "tux64/arguments.h"
 
 #include <tux64/memory.h>
+#include <tux64/bitwise.h>
 
 void
 tux64_arguments_iterator_initialize_command_line(
@@ -73,6 +74,118 @@ tux64_arguments_iterator_next(
    return tux64_arguments_iterator_next_functions[(Tux64UInt8)self->type](self);
 }
 
+static void
+tux64_arguments_parse_required_storage_initialize(
+   const struct Tux64ArgumentsList * list,
+   Tux64UInt8 * required_storage
+) {
+   Tux64UInt32 bytes;
+   Tux64UInt8 sentinel;
+
+   /* have to do a ceil-divide instead of floor-divide */
+   bytes = (list->options_required_count + TUX64_LITERAL_UINT32(8u)) / TUX64_LITERAL_UINT32(8u);
+   sentinel = 0x00;
+
+   tux64_memory_fill(
+      required_storage,
+      &sentinel,
+      bytes,
+      TUX64_LITERAL_UINT32(sizeof(sentinel))
+   );
+
+   return;
+}
+
+static struct Tux64ArgumentsParseResult
+tux64_arguments_parse_check_required_error_missing(
+   const struct Tux64ArgumentsList * list,
+   Tux64UInt32 argument_index,
+   Tux64UInt32 flags
+) {
+   struct Tux64ArgumentsParseResult result;
+   const struct Tux64ArgumentsOption * option;
+
+   /* find the true argument index for the lowest set bit */
+   while ((flags & TUX64_LITERAL_UINT32(1u)) == TUX64_LITERAL_UINT32(1u)) {
+      argument_index++;
+      flags >>= TUX64_LITERAL_UINT8(1u);
+   }
+
+   /* get the relevant option */
+   option = &list->options_required[argument_index];
+
+   /* choose the first available identifier and return */
+   result.status = TUX64_ARGUMENTS_PARSE_STATUS_REQUIRED_MISSING;
+   result.payload.required_missing.identifier = option->identifiers_long[0u];
+   return result;
+}
+
+static struct Tux64ArgumentsParseResult
+tux64_arguments_parse_check_required(
+   const struct Tux64ArgumentsList * list,
+   const Tux64UInt8 * required_storage
+) {
+   struct Tux64ArgumentsParseResult result;
+   Tux64UInt32 full_bytes;
+   Tux64UInt32 remaining_bits;
+   Tux64UInt32 argument_index;
+   Tux64UInt8 flags;
+   Tux64UInt8 mask;
+   const Tux64UInt8 * iter_required_storage;
+
+   full_bytes              = list->options_required_count / TUX64_LITERAL_UINT32(8u);
+   remaining_bits          = list->options_required_count % TUX64_LITERAL_UINT32(8u);
+   iter_required_storage   = required_storage;
+   argument_index          = TUX64_LITERAL_UINT32(0u);
+
+   /* check full bytes at once*/
+   while (full_bytes != TUX64_LITERAL_UINT32(0u)) {
+      flags = *iter_required_storage;
+
+      if (flags != TUX64_LITERAL_UINT8(0xff)) {
+         return tux64_arguments_parse_check_required_error_missing(list, argument_index, flags);
+      }
+
+      argument_index += TUX64_LITERAL_UINT32(8u);
+      iter_required_storage++;
+      full_bytes--;
+   }
+
+   /* check any remaining bits */
+   if (remaining_bits != TUX64_LITERAL_UINT32(0u)) {
+      flags = *iter_required_storage;
+
+      mask = tux64_bitwise_mask_set_all_low_uint8(remaining_bits);
+
+      if (tux64_bitwise_flags_check_all_uint8(mask, flags) == TUX64_BOOLEAN_FALSE) {
+         return tux64_arguments_parse_check_required_error_missing(list, argument_index, flags);
+      }
+   }
+
+   result.status = TUX64_ARGUMENTS_PARSE_STATUS_OK;
+   return result;
+}
+
+static struct Tux64ArgumentsParseResult
+tux64_arguments_parse_argument(
+   const struct Tux64ArgumentsList * list,
+   struct Tux64ArgumentsIterator * iterator,
+   void * context,
+   Tux64UInt8 * required_storage,
+   const struct Tux64ArgumentsIteratorNextPayloadOk * argument
+) {
+   struct Tux64ArgumentsParseResult result;
+
+   /* TODO: implement */
+   (void)list;
+   (void)iterator;
+   (void)context;
+   (void)required_storage;
+   (void)argument;
+   result.status = TUX64_ARGUMENTS_PARSE_STATUS_OK;
+   return result;
+}
+
 struct Tux64ArgumentsParseResult
 tux64_arguments_parse(
    const struct Tux64ArgumentsList * list,
@@ -81,16 +194,30 @@ tux64_arguments_parse(
    Tux64UInt8 * required_storage
 ) {
    struct Tux64ArgumentsParseResult result;
+   struct Tux64ArgumentsIteratorNextResult argument;
 
-   /* TODO: implement*/
-   (void)list;
-   (void)iterator;
-   (void)context;
-   (void)required_storage;
-   result.status = TUX64_ARGUMENTS_PARSE_STATUS_PARAMETER_INVALID;
-   result.payload.parameter_invalid.identifier = "--fuck-rust";
-   result.payload.parameter_invalid.parameter = "true";
-   result.payload.parameter_invalid.reason = "assignment to tautology is redundant";
+   /* zero-initialize required_storage */
+   tux64_arguments_parse_required_storage_initialize(list, required_storage);
+
+   /* parse each argument individually */
+   argument = tux64_arguments_iterator_next(iterator);
+   while (argument.status == TUX64_ARGUMENTS_ITERATOR_NEXT_STATUS_OK) {
+      result = tux64_arguments_parse_argument(
+         list,
+         iterator,
+         context,
+         required_storage,
+         &argument.payload.ok
+      );
+      if (result.status != TUX64_ARGUMENTS_PARSE_STATUS_OK) {
+         return result;
+      }
+
+      argument = tux64_arguments_iterator_next(iterator);
+   }
+
+   /* check that all required arguments were present and GTFO */
+   result = tux64_arguments_parse_check_required(list, required_storage);
    return result;
 }
 
