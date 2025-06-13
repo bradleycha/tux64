3853243a79ed1e6acf43ba82c1e4b7efa1203f5d
Chase Bradley <bradleycha@proton.me>
2025-06-13 18:24:35 -0400

    tux64-lib: add filesystem functions to load and save files to and from memory

---
+++ lib/src/tux64/fs.c
@@ -0,0 +1,247 @@
+/*----------------------------------------------------------------------------*/
+/*                          Copyright (C) Tux64 2025                          */
+/*                    https://github.com/bradleycha/tux64                     */
+/*----------------------------------------------------------------------------*/
+/* lib/src/tux64/fs.c - Implementations for filesystem functions.             */
+/*----------------------------------------------------------------------------*/
+
+#include "tux64/tux64.h"
+#include "tux64/fs.h"
+
+#if _TUX64_FS_ENABLE
+/*----------------------------------------------------------------------------*/
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <errno.h>
+
+static enum Tux64FsStatus
+tux64_fs_errno_to_fs_status(
+   int err
+) {
+   enum Tux64FsStatus status;
+
+   /* "errno" - i can't believe it's not local! */
+   /* ... */
+   /* seriously, what the FUCK were the C standard developers thinking with */
+   /* this bullshit interface? just return an enum for fucks sake...but NO! */
+   /* instead we get this fucked-up shit where we return NULL then set a */
+   /* GLOBAL VARIABLE that *LOOKS* like a local variable... */
+   /* also, the entire stdio header is a horrible abortion of an API. */
+
+   switch (err) {
+      case 0:
+         /* we should only call this function when an error ocurred.  if */
+         /* errno is set to zero when we failed, then file a bug-report to */
+         /* the assholes who wrote this horrendous interface. */
+         TUX64_UNREACHABLE;
+
+      case ENOENT:
+         status = TUX64_FS_STATUS_NOT_FOUND;
+         break;
+
+      case EACCES:
+         /* these people are so stupid, they can't even spell their OWN */
+         /* damn shiterface correctly! */
+         status = TUX64_FS_STATUS_PERMISSION_DENIED;
+         break;
+
+      case EISDIR:
+         status = TUX64_FS_STATUS_NOT_A_FILE;
+         break;
+
+      case ENOMEM:
+         /* hurr-durr, i'm a fuckwit who eats sand and drinks piss.  i also */
+         /* use fallible memory allocators for I/O operations which have NO */
+         /* reason to allocate memory. */
+         status = TUX64_FS_STATUS_OUT_OF_MEMORY;
+         break;
+
+      case EROFS:
+         status = TUX64_FS_STATUS_PERMISSION_DENIED;
+         break;
+
+      case ENOSPC:
+         status = TUX64_FS_STATUS_OUT_OF_MEMORY;
+         break;
+
+      default:
+         /* do we document what can be returned? do we use an enum? NO! libc */
+         /* just returns some random shit and says "best of luck, fucker! */
+         /* good luck parsing this crap!" */
+         status = TUX64_FS_STATUS_UNKNOWN_ERROR;
+         break;
+   }
+
+   return status;
+}
+
+static struct Tux64FsFileLoadResult
+tux64_fs_file_load_allocated(
+   FILE * file,
+   Tux64UInt8 * read_buffer,
+   Tux64UInt32 read_bytes_total
+) {
+   struct Tux64FsFileLoadResult result;
+   Tux64UInt8 * iter_read_buffer;
+   Tux64UInt32 read_bytes_remaining;
+   Tux64UInt32 read_bytes;
+
+   iter_read_buffer = read_buffer;
+   read_bytes_remaining = read_bytes_total;
+
+   while (read_bytes_remaining != TUX64_LITERAL_UINT32(0u)) {
+      read_bytes = (Tux64UInt32)fread(
+         iter_read_buffer,
+         (size_t)sizeof(Tux64UInt8),
+         read_bytes_remaining,
+         file
+      );
+      if (ferror(file) != 0) {
+         result.status = tux64_fs_errno_to_fs_status(errno);
+         return result;
+      }
+
+      iter_read_buffer += read_bytes;
+      read_bytes_remaining -= read_bytes;
+   }
+
+   result.status = TUX64_FS_STATUS_OK;
+   result.payload.ok.data = read_buffer;
+   result.payload.ok.bytes = read_bytes_total;
+   return result;
+}
+
+static struct Tux64FsFileLoadResult
+tux64_fs_file_load_open(
+   FILE * file
+) {
+   struct Tux64FsFileLoadResult result;
+   long read_bytes;
+   Tux64UInt8 * read_buffer;
+
+   /* this ridiculous code is required just to get the number of bytes in a */
+   /* file. */
+   if (fseek(file, 0, SEEK_END) != 0) {
+      result.status = tux64_fs_errno_to_fs_status(errno);
+      return result;
+   }
+   read_bytes = ftell(file);
+   if (read_bytes < 0) {
+      result.status = tux64_fs_errno_to_fs_status(errno);
+      return result;
+   }
+   rewind(file);
+
+   /* now we check for differing types to overflow since everything is */
+   /* incompatible and of non-standard size */
+#if TUX64_SIZEOF_LONG > 4
+   if ((unsigned long)read_bytes > TUX64_UINT32_MAX) {
+      result.status = TUX64_FS_STATUS_OUT_OF_MEMORY;
+      return result;
+   }
+#endif /* TUX64_SIZEOF_LONG > 4 */
+
+   /* we check again just for good measure */
+   if ((unsigned long)read_bytes > SIZE_MAX) {
+      result.status = TUX64_FS_STATUS_OUT_OF_MEMORY;
+      return result;
+   }
+
+   /* and now we allocate some bullshit for the file contents */
+   read_buffer = (Tux64UInt8 *)malloc((size_t)read_bytes);
+   if (read_buffer == NULL) {
+      result.status = TUX64_FS_STATUS_OUT_OF_MEMORY;
+      return result;
+   }
+
+   /* and we continue to actually loading the file, freeing the bullshit */
+   /* if we encounter an error.  so basically we implement errdefer from zig. */
+   result = tux64_fs_file_load_allocated(
+      file,
+      read_buffer,
+      (Tux64UInt32)read_bytes
+   );
+   if (result.status != TUX64_FS_STATUS_OK) {
+      free(read_buffer);
+   }
+   return result;
+}
+
+struct Tux64FsFileLoadResult
+tux64_fs_file_load(
+   const char * path
+) {
+   struct Tux64FsFileLoadResult result;
+   FILE * f;
+
+   f = fopen(path, "r");
+   if (f == NULL) {
+      result.status = tux64_fs_errno_to_fs_status(errno);
+      return result;
+   }
+
+   result = tux64_fs_file_load_open(f);
+   (void)fclose(f);
+   return result;
+}
+
+static enum Tux64FsStatus
+tux64_fs_file_save_open(
+   FILE * file_dest,
+   const struct Tux64FsLoadedFile * file_src
+) {
+   const Tux64UInt8 * iter_write_buffer;
+   Tux64UInt32 write_bytes_remaining;
+   Tux64UInt32 written_bytes;
+
+   iter_write_buffer = file_src->data;
+   write_bytes_remaining = file_src->bytes;
+
+   while (write_bytes_remaining != TUX64_LITERAL_UINT32(0u)) {
+      written_bytes = fwrite(
+         iter_write_buffer,
+         (size_t)sizeof(Tux64UInt8),
+         write_bytes_remaining,
+         file_dest
+      );
+      if (ferror(file_dest) != 0) {
+         return tux64_fs_errno_to_fs_status(errno);
+      }
+
+      iter_write_buffer += written_bytes;
+      write_bytes_remaining -= written_bytes;
+   }
+
+   return TUX64_FS_STATUS_OK;
+}
+
+enum Tux64FsStatus
+tux64_fs_file_save(
+   const char * path,
+   const struct Tux64FsLoadedFile * file
+) {
+   enum Tux64FsStatus status;
+   FILE * file_dest;
+
+   file_dest = fopen(path, "w");
+   if (file_dest != NULL) {
+      status = tux64_fs_file_save_open(file_dest, file);
+      (void)fclose(file_dest);
+      return status;
+   }
+
+   return tux64_fs_errno_to_fs_status(errno);
+}
+
+void
+tux64_fs_file_unload(
+   struct Tux64FsLoadedFile * file
+) {
+   free(file->data);
+   return;
+}
+
+/*----------------------------------------------------------------------------*/
+#endif /* _TUX64_FS_ENABLE */
+
---
+++ lib/src/tux64/fs.h
@@ -0,0 +1,74 @@
+/*----------------------------------------------------------------------------*/
+/*                          Copyright (C) Tux64 2025                          */
+/*                    https://github.com/bradleycha/tux64                     */
+/*----------------------------------------------------------------------------*/
+/* lib/src/tux64/fs.h - Header for filesystem functions.                      */
+/*----------------------------------------------------------------------------*/
+
+#ifndef _TUX64_FS_H
+#define _TUX64_FS_H
+/*----------------------------------------------------------------------------*/
+
+#include "tux64/tux64.h"
+
+#define _TUX64_FS_ENABLE\
+   (TUX64_HAVE_STDLIB_H && TUX64_HAVE_STDIO_H && TUX64_HAVE_ERRNO_H)
+
+#if _TUX64_FS_ENABLE
+/*----------------------------------------------------------------------------*/
+
+enum Tux64FsStatus {
+   TUX64_FS_STATUS_OK,
+   TUX64_FS_STATUS_NOT_FOUND,
+   TUX64_FS_STATUS_PERMISSION_DENIED,
+   TUX64_FS_STATUS_NOT_A_FILE,
+   TUX64_FS_STATUS_OUT_OF_MEMORY,
+   TUX64_FS_STATUS_UNKNOWN_ERROR
+};
+
+struct Tux64FsLoadedFile {
+   Tux64UInt8 * data;
+   Tux64UInt32 bytes;
+};
+
+union Tux64FsFileLoadPayload {
+   struct Tux64FsLoadedFile ok;
+};
+
+struct Tux64FsFileLoadResult {
+   enum Tux64FsStatus status;
+   union Tux64FsFileLoadPayload payload;
+};
+
+/*----------------------------------------------------------------------------*/
+/* Attempts to load a file into memory from the given path.  The file must be */
+/* freed using tux64_fs_file_unload() once no longer needed.                  */
+/*----------------------------------------------------------------------------*/
+struct Tux64FsFileLoadResult
+tux64_fs_file_load(
+   const char * path
+);
+
+/*----------------------------------------------------------------------------*/
+/* Attempts to write a loaded file to the given path.                         */
+/*----------------------------------------------------------------------------*/
+enum Tux64FsStatus
+tux64_fs_file_save(
+   const char * path,
+   const struct Tux64FsLoadedFile * file
+);
+
+/*----------------------------------------------------------------------------*/
+/* Frees the memory associated with a loaded file.                            */
+/*----------------------------------------------------------------------------*/
+void
+tux64_fs_file_unload(
+   struct Tux64FsLoadedFile * file
+);
+
+/*----------------------------------------------------------------------------*/
+#endif /* _TUX64_FS_ENABLE */
+
+/*----------------------------------------------------------------------------*/
+#endif /* _TUX64_FS_H */
+
