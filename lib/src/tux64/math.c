46b675de90a5d140110c43499fabbe8ca4723fd9
Chase Bradley <bradleycha@proton.me>
2025-06-20 22:48:56 -0400

    tux64-lib: fix bugged implementation of forward alignment where already aligned values were incremented

--- lib/src/tux64/math.c
+++ lib/src/tux64/math.c
@@ -9,43 +9,11 @@
 #include "tux64/math.h"
 
 Tux64UInt8
-tux64_math_align_forward_uint8(
-   Tux64UInt8 value,
-   Tux64UInt8 alignment
-) {
-   return TUX64_MATH_ALIGN_FORWARD(value, alignment);
-}
-
-Tux64UInt16
-tux64_math_align_forward_uint16(
-   Tux64UInt16 value,
-   Tux64UInt16 alignment
-) {
-   return TUX64_MATH_ALIGN_FORWARD(value, alignment);
-}
-
-Tux64UInt32
-tux64_math_align_forward_uint32(
-   Tux64UInt32 value,
-   Tux64UInt32 alignment
-) {
-   return TUX64_MATH_ALIGN_FORWARD(value, alignment);
-}
-
-Tux64UInt64
-tux64_math_align_forward_uint64(
-   Tux64UInt64 value,
-   Tux64UInt64 alignment
-) {
-   return TUX64_MATH_ALIGN_FORWARD(value, alignment);
-}
-
-Tux64UInt8
 tux64_math_ceil_divide_uint8(
    Tux64UInt8 dividend,
    Tux64UInt8 divisor
 ) {
-   return tux64_math_ceil_divide_uint64(
+   return (Tux64UInt8)tux64_math_ceil_divide_uint64(
       (Tux64UInt64)dividend,
       (Tux64UInt64)divisor
    );
@@ -56,7 +24,7 @@ tux64_math_ceil_divide_uint16(
    Tux64UInt16 dividend,
    Tux64UInt16 divisor
 ) {
-   return tux64_math_ceil_divide_uint64(
+   return (Tux64UInt16)tux64_math_ceil_divide_uint64(
       (Tux64UInt64)dividend,
       (Tux64UInt64)divisor
    );
@@ -67,7 +35,7 @@ tux64_math_ceil_divide_uint32(
    Tux64UInt32 dividend,
    Tux64UInt32 divisor
 ) {
-   return tux64_math_ceil_divide_uint64(
+   return (Tux64UInt32)tux64_math_ceil_divide_uint64(
       (Tux64UInt64)dividend,
       (Tux64UInt64)divisor
    );
@@ -83,5 +51,46 @@ tux64_math_ceil_divide_uint64(
    }
 
    return TUX64_LITERAL_UINT64(1llu) + ((dividend - 1) / divisor);
+}
+
+Tux64UInt8
+tux64_math_align_forward_uint8(
+   Tux64UInt8 value,
+   Tux64UInt8 alignment
+) {
+   return (Tux64UInt8)tux64_math_align_forward_uint64(
+      (Tux64UInt64)value,
+      (Tux64UInt64)alignment
+   );
+}
+
+Tux64UInt16
+tux64_math_align_forward_uint16(
+   Tux64UInt16 value,
+   Tux64UInt16 alignment
+) {
+   return (Tux64UInt16)tux64_math_align_forward_uint64(
+      (Tux64UInt64)value,
+      (Tux64UInt64)alignment
+   );
+}
+
+Tux64UInt32
+tux64_math_align_forward_uint32(
+   Tux64UInt32 value,
+   Tux64UInt32 alignment
+) {
+   return (Tux64UInt32)tux64_math_align_forward_uint64(
+      (Tux64UInt64)value,
+      (Tux64UInt64)alignment
+   );
+}
+
+Tux64UInt64
+tux64_math_align_forward_uint64(
+   Tux64UInt64 value,
+   Tux64UInt64 alignment
+) {
+   return tux64_math_ceil_divide_uint64(value, alignment) * alignment;
 }
 
--- lib/src/tux64/math.h
+++ lib/src/tux64/math.h
@@ -12,36 +12,11 @@
 #include "tux64/tux64.h"
 
 /*----------------------------------------------------------------------------*/
-/* Aligns a value forward to a boundary on literals.  Only use this if you    */
-/* can't use the non-preprocessor functions, for example in #define           */
-/* statements.                                                                */
+/* Performs integer ceiling divison on literals.  Only use this if you can't  */
+/* use the non-preprocessor functions, for example in #define statements.     */
 /*----------------------------------------------------------------------------*/
-#define TUX64_MATH_ALIGN_FORWARD(value, alignment)\
-   (value + alignment - (value % alignment))
-
-/*----------------------------------------------------------------------------*/
-/* Aligns a value forward to a boundary.                                      */
-/*----------------------------------------------------------------------------*/
-Tux64UInt8
-tux64_math_align_forward_uint8(
-   Tux64UInt8 value,
-   Tux64UInt8 alignment
-);
-Tux64UInt16
-tux64_math_align_forward_uint16(
-   Tux64UInt16 value,
-   Tux64UInt16 alignment
-);
-Tux64UInt32
-tux64_math_align_forward_uint32(
-   Tux64UInt32 value,
-   Tux64UInt32 alignment
-);
-Tux64UInt64
-tux64_math_align_forward_uint64(
-   Tux64UInt64 value,
-   Tux64UInt64 alignment
-);
+#define TUX64_MATH_CEIL_DIVIDE(dividend, divisor)\
+   (((dividend) + (divisor) - 1u) / (divisor))
 
 /*----------------------------------------------------------------------------*/
 /* Performs integer ceiling division.                                         */
@@ -65,6 +40,38 @@ Tux64UInt64
 tux64_math_ceil_divide_uint64(
    Tux64UInt64 dividend,
    Tux64UInt64 divisor
+);
+
+/*----------------------------------------------------------------------------*/
+/* Aligns a value forward to a boundary on literals.  Only use this if you    */
+/* can't use the non-preprocessor functions, for example in #define           */
+/* statements.                                                                */
+/*----------------------------------------------------------------------------*/
+#define TUX64_MATH_ALIGN_FORWARD(value, alignment)\
+   (TUX64_MATH_CEIL_DIVIDE(value, alignment) * (alignment))
+
+/*----------------------------------------------------------------------------*/
+/* Aligns a value forward to a boundary.                                      */
+/*----------------------------------------------------------------------------*/
+Tux64UInt8
+tux64_math_align_forward_uint8(
+   Tux64UInt8 value,
+   Tux64UInt8 alignment
+);
+Tux64UInt16
+tux64_math_align_forward_uint16(
+   Tux64UInt16 value,
+   Tux64UInt16 alignment
+);
+Tux64UInt32
+tux64_math_align_forward_uint32(
+   Tux64UInt32 value,
+   Tux64UInt32 alignment
+);
+Tux64UInt64
+tux64_math_align_forward_uint64(
+   Tux64UInt64 value,
+   Tux64UInt64 alignment
 );
 
 /*----------------------------------------------------------------------------*/
