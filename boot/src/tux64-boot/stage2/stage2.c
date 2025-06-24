9316c34bf52af506cba01c21563fe1f2f8df52da
Chase Bradley <bradleycha@proton.me>
2025-06-23 23:18:54 -0400

    tux64-boot: update for changed structure for tux64-lib

--- boot/src/tux64-boot/stage2/stage2.c
+++ boot/src/tux64-boot/stage2/stage2.c
@@ -7,19 +7,19 @@
 
 #include "tux64-boot/tux64-boot.h"
 
-#include <tux64/platform-n64/boot.h>
+#include <tux64/platform/mips/n64/boot.h>
 #include "tux64-boot/stage3/payload.h"
 
 void
 tux64_boot_stage2_start(
-   const struct Tux64PlatformN64BootHeader *,
+   const struct Tux64PlatformMipsN64BootHeader *,
    Tux64UInt32
 )
 __attribute__((noreturn, section(".start")));
 
 void
 tux64_boot_stage2_start(
-   const struct Tux64PlatformN64BootHeader * boot_header,
+   const struct Tux64PlatformMipsN64BootHeader * boot_header,
    Tux64UInt32 total_memory
 ) {
    /* TODO: implement */
