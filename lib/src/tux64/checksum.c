da7a12d175deacd34ab943f732255c283e97b63d
Chase Bradley <bradleycha@proton.me>
2025-06-24 21:37:58 -0400

    tux64-lib: add interface for custom fletcher64-based checksum

--- lib/src/tux64/checksum.c
+++ lib/src/tux64/checksum.c
@@ -8,3 +8,42 @@
 #include "tux64/tux64.h"
 #include "tux64/checksum.h"
 
+static void
+tux64_checksum_fletcher_64_32_initialize(
+   struct Tux64ChecksumContext * context
+) {
+   /* TODO: implement */
+   (void)context;
+   return;
+}
+
+static void
+tux64_checksum_fletcher_64_32_digest(
+   struct Tux64ChecksumContext * context,
+   const Tux64UInt8 * data,
+   Tux64UInt32 bytes
+) {
+   /* TODO: implement */
+   (void)context;
+   (void)data;
+   (void)bytes;
+   return;
+}
+
+static Tux64UInt8 *
+tux64_checksum_fletcher_64_32_finalize(
+   struct Tux64ChecksumContext * context
+) {
+   /* TODO: implement */
+   (void)context;
+   return TUX64_NULLPTR;
+}
+
+const struct Tux64ChecksumFunction
+tux64_checksum_fletcher_64_32 = {
+   .initialize    = tux64_checksum_fletcher_64_32_initialize,
+   .digest        = tux64_checksum_fletcher_64_32_digest,
+   .finalize      = tux64_checksum_fletcher_64_32_finalize,
+   .digest_bytes  = TUX64_LITERAL_UINT8(TUX64_CHECKSUM_FLETCHER_64_32_DIGEST_BYTES)
+};
+
--- lib/src/tux64/checksum.h
+++ lib/src/tux64/checksum.h
@@ -11,6 +11,67 @@
 
 #include "tux64/tux64.h"
 
+struct Tux64ChecksumContext;
+
+/*----------------------------------------------------------------------------*/
+/* Initializes the checksum context for the given function.                   */
+/*----------------------------------------------------------------------------*/
+typedef void (*Tux64ChecksumFunctionInitialize)(
+   struct Tux64ChecksumContext * context
+);
+
+/*----------------------------------------------------------------------------*/
+/* Digests byte data into the checksum function.  'context' should have been  */
+/* first initialized before digesting.                                        */
+/*----------------------------------------------------------------------------*/
+typedef void (*Tux64ChecksumFunctionDigest)(
+   struct Tux64ChecksumContext * context,
+   const Tux64UInt8 * data,
+   Tux64UInt32 bytes
+);
+
+/*----------------------------------------------------------------------------*/
+/* Finalizes the checksum context, returning a pointer to the final digest    */
+/* value.  The returned digest value pointer is valid until 'context' is      */
+/* re-initialized.                                                            */
+/*----------------------------------------------------------------------------*/
+typedef Tux64UInt8 * (*Tux64ChecksumFunctionFinalize)(
+   struct Tux64ChecksumContext * context
+);
+
+struct Tux64ChecksumFunction {
+   Tux64ChecksumFunctionInitialize initialize;
+   Tux64ChecksumFunctionDigest digest;
+   Tux64ChecksumFunctionFinalize finalize;
+   Tux64UInt8 digest_bytes;
+};
+
+/*----------------------------------------------------------------------------*/
+/* A variation of the Fletcher64 algorithm.  Words are treated as big-endian, */
+/* and the returned digest value is a 32-bit word that is the result of       */
+/* performing wrapping subtraction of the second sum minus the first sum.     */
+/* The modulus used is 2^32.                                                  */
+/*----------------------------------------------------------------------------*/
+#define TUX64_CHECKSUM_FLETCHER_64_32_DIGEST_BYTES\
+   4u
+extern const struct Tux64ChecksumFunction
+tux64_checksum_fletcher_64_32;
+
+struct _Tux64ChecksumContextAlgorithmFletcher6432 {
+   Tux64UInt32 sum_hi;
+   Tux64UInt32 sum_lo;
+   Tux64UInt8 word_buffer [sizeof(Tux64UInt32)];
+   Tux64UInt8 word_buffer_capacity;
+};
+
+union _Tux64ChecksumContextAlgorithm {
+   struct _Tux64ChecksumContextAlgorithmFletcher6432 fletcher_64_32;
+};
+
+struct Tux64ChecksumContext {
+   union _Tux64ChecksumContextAlgorithm algorithm;
+};
+
 /*----------------------------------------------------------------------------*/
 #endif /* _TUX64_CHECKSUM_H */
 
