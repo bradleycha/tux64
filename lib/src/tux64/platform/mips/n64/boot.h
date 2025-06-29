deba9e9c4685eff5149a217e17a1443f2f0cd429
Chase Bradley <bradleycha@proton.me>
2025-06-28 20:53:00 -0400

    tux64-lib: rework boot header to get rid of version and dynamic length

--- lib/src/tux64/platform/mips/n64/boot.h
+++ lib/src/tux64/platform/mips/n64/boot.h
@@ -31,65 +31,41 @@ struct Tux64PlatformMipsN64BootHeaderVersion {
 #define TUX64_PLATFORM_MIPS_N64_BOOT_FLAG_NO_CHECKSUM\
    (1u << 0u)
 
-struct Tux64PlatformMipsN64BootHeaderFileBootloaderStage1 {
-   Tux64UInt32 checksum;
-   Tux64UInt32 length_words_code_data;
-   Tux64UInt32 length_words_bss;
-};
-
-struct Tux64PlatformMipsN64BootHeaderFileBootloaderStage2 {
-   Tux64UInt32 checksum;
-   Tux64UInt32 length_words_code_data;
-};
-
-struct Tux64PlatformMipsN64BootHeaderFileKernel {
-   Tux64UInt32 checksum;
-   Tux64UInt32 length_words_code_data;
-   Tux64UInt32 length_words_bss;
-};
-
-struct Tux64PlatformMipsN64BootHeaderFileInitramfs {
-   Tux64UInt32 checksum;
-   Tux64UInt32 length_words;
-};
-
-struct Tux64PlatformMipsN64BootHeaderFileCommandLine {
+struct Tux64PlatformMipsN64BootHeaderFile {
    Tux64UInt32 checksum;
    Tux64UInt32 length_words;
 };
 
 struct Tux64PlatformMipsN64BootHeaderFileBootloader {
-   struct Tux64PlatformMipsN64BootHeaderFileBootloaderStage1 stage1;
-   struct Tux64PlatformMipsN64BootHeaderFileBootloaderStage2 stage2;
+   struct Tux64PlatformMipsN64BootHeaderFile stage1;
+   struct Tux64PlatformMipsN64BootHeaderFile stage2;
 };
 
 struct Tux64PlatformMipsN64BootHeaderFiles {
    struct Tux64PlatformMipsN64BootHeaderFileBootloader bootloader;
-   struct Tux64PlatformMipsN64BootHeaderFileKernel kernel;
-   struct Tux64PlatformMipsN64BootHeaderFileInitramfs initramfs;
-   struct Tux64PlatformMipsN64BootHeaderFileCommandLine command_line;
+   struct Tux64PlatformMipsN64BootHeaderFile kernel;
+   struct Tux64PlatformMipsN64BootHeaderFile initramfs;
+   struct Tux64PlatformMipsN64BootHeaderFile command_line;
 };
+
+struct Tux64PlatformMipsN64BootHeaderData {
+   /* bitflags used by various stages of the boot process */
+   Tux64UInt32 flags;
+
+   /* files used during the boot process */
+   struct Tux64PlatformMipsN64BootHeaderFiles files;
+}
 
 /* the boot header stored after IPL3/stage-0 in ROM. */
 struct Tux64PlatformMipsN64BootHeader {
    /* 4-byte magic value, used to quickly verify the boot header is present */
    Tux64UInt8 magic [TUX64_PLATFORM_MIPS_N64_BOOT_HEADER_MAGIC_BYTES];
 
-   /* checksum of the entire header, minus the magic value, used to verify */
-   /* the header isn't corrupted */
+   /* checksum of the 'data' field */
    Tux64UInt32 checksum;
 
-   /* the bootloader version associated with the header */
-   struct Tux64PlatformMipsN64BootHeaderVersion version;
-
-   /* the length of the header, in words */
-   Tux64UInt32 length_words;
-
-   /* bitflags used by various stages of the boot process */
-   Tux64UInt32 flags;
-
-   /* files used during the boot process */
-   struct Tux64PlatformMipsN64BootHeaderFiles files;
+   /* boot header data which is protected by the checksum */
+   struct Tux64PlatformMipsN64BootHeaderData data; 
 };
 
 /*----------------------------------------------------------------------------*/
