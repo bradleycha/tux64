0221c14b716f91fb183f6660c11c191211c1f8ba
Chase Bradley <bradleycha@proton.me>
2025-06-30 13:08:08 -0400

    tux64-scripts: add -flto to default CFLAGS

--- scripts/buildconf.sh
+++ scripts/buildconf.sh
@@ -20,12 +20,12 @@ export TUX64_TARGET_HOST=
 export TUX64_TARGET_N64_KERNEL=mips64-elf
 export TUX64_TARGET_N64_USERSPACE=mips64-linux-musl
 
-export TUX64_CFLAGS_HOST="-pipe -march=native -O2"
+export TUX64_CFLAGS_HOST="-pipe -march=native -O2 -flto"
 export TUX64_CXXFLAGS_HOST="${TUX64_CFLAGS_HOST}"
 export TUX64_ASFLAGS_HOST=""
 export TUX64_LDFLAGS_HOST="-flto -Wl,--gc-sections -s"
 
-export TUX64_CFLAGS_N64_COMMON="-pipe -march=vr4300 -mfix4300 -mabi=o64 -Oz -fno-stack-protector -fomit-frame-pointer -ffunction-sections -fdata-sections"
+export TUX64_CFLAGS_N64_COMMON="-pipe -march=vr4300 -mfix4300 -mabi=o64 -Oz -flto -fno-stack-protector -fomit-frame-pointer -ffunction-sections -fdata-sections"
 export TUX64_CXXFLAGS_N64_COMMON="${TUX64_CFLAGS_N64_COMMON}"
 export TUX64_ASFLAGS_N64_COMMON="-march=vr4300 -mtune=vr4300"
 export TUX64_LDFLAGS_N64_COMMON="-flto -Wl,--gc-sections -s"
