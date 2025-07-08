52fa092c6831883766988364cad9de42a333f08c
Chase Bradley <bradleycha@proton.me>
2025-07-08 17:08:54 -0400

    tux64-boot: create files for stage-1 video system

---
+++ boot/src/tux64-boot/stage1/video.c
@@ -0,0 +1,21 @@
+/*----------------------------------------------------------------------------*/
+/*                          Copyright (C) Tux64 2025                          */
+/*                    https://github.com/bradleycha/tux64                     */
+/*----------------------------------------------------------------------------*/
+/* boot/src/tux64-boot/stage1/video.c - Framebuffer video implementation.     */
+/*----------------------------------------------------------------------------*/
+
+#include "tux64-boot/tux64-boot.h"
+
+void
+tux64_boot_stage1_video_initialize(void) {
+   /* TODO: implement */
+   return;
+}
+
+void
+tux64_boot_stage1_video_swap_buffers(void) {
+   /* TODO: implement */
+   return;
+}
+
