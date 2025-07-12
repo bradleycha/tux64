933d163e4734c3d74db3f0aa5ed72c7871bf41ea
Chase Bradley <bradleycha@proton.me>
2025-07-11 22:54:32 -0400

    tux64-boot: update for previous tux64-lib commit

--- boot/src/tux64-boot/tux64-boot.h
+++ boot/src/tux64-boot/tux64-boot.h
@@ -12,9 +12,9 @@
 #include <tux64/tux64.h>
 #include "tux64-boot/config.h"
 
-#if !TUX64_CONFIG_PLATFORM_N64
-#error "tux64-lib has TUX64_CONFIG_PLATFORM_N64 disabled.  please rebuild tux64-lib with --enable-platform-n64."
-#endif /* !TUX64_CONFIG_PLATFORM_N64 */
+#if !TUX64_CONFIG_PLATFORM_MIPS_N64
+#error "tux64-lib has TUX64_CONFIG_PLATFORM_MIPS_N64 disabled.  please rebuild tux64-lib with --enable-platform-n64."
+#endif /* !TUX64_CONFIG_PLATFORM_MIPS_N64 */
 
 /*----------------------------------------------------------------------------*/
 #endif /* _TUX64_BOOT_H */
