498906c8d5ccd55fb07a3f1042f00f5ea15773ed
Chase Bradley <bradleycha@proton.me>
2025-07-14 19:48:44 -0400

    tux64-lib: add co-processor 0 register read/write functions

--- lib/src/tux64/platform/mips/vr4300/cop0.c
+++ lib/src/tux64/platform/mips/vr4300/cop0.c
@@ -14,7 +14,669 @@
 #if TUX64_PLATFORM_MIPS_VR4300_ENABLE
 /*----------------------------------------------------------------------------*/
 
+/* these are all string literals so we can use them in assembler template */
+/* strings. */
+#define TUX64_PLATFORM_MIPS_VR4300_COP0_REGISTER_INDEX\
+   "0"
+#define TUX64_PLATFORM_MIPS_VR4300_COP0_REGISTER_RANDOM\
+   "1"
+#define TUX64_PLATFORM_MIPS_VR4300_COP0_REGISTER_ENTRYLO0\
+   "2"
+#define TUX64_PLATFORM_MIPS_VR4300_COP0_REGISTER_ENTRYLO1\
+   "3"
+#define TUX64_PLATFORM_MIPS_VR4300_COP0_REGISTER_CONTEXT\
+   "4"
+#define TUX64_PLATFORM_MIPS_VR4300_COP0_REGISTER_PAGEMASK\
+   "5"
+#define TUX64_PLATFORM_MIPS_VR4300_COP0_REGISTER_WIRED\
+   "6"
+#define TUX64_PLATFORM_MIPS_VR4300_COP0_REGISTER_BADVADDR\
+   "8"
+#define TUX64_PLATFORM_MIPS_VR4300_COP0_REGISTER_COUNT\
+   "9"
+#define TUX64_PLATFORM_MIPS_VR4300_COP0_REGISTER_ENTRYHI\
+   "10"
+#define TUX64_PLATFORM_MIPS_VR4300_COP0_REGISTER_COMPARE\
+   "11"
+#define TUX64_PLATFORM_MIPS_VR4300_COP0_REGISTER_STATUS\
+   "12"
+#define TUX64_PLATFORM_MIPS_VR4300_COP0_REGISTER_CAUSE\
+   "13"
+#define TUX64_PLATFORM_MIPS_VR4300_COP0_REGISTER_EPC\
+   "14"
+#define TUX64_PLATFORM_MIPS_VR4300_COP0_REGISTER_PRID\
+   "15"
+#define TUX64_PLATFORM_MIPS_VR4300_COP0_REGISTER_CONFIG\
+   "16"
+#define TUX64_PLATFORM_MIPS_VR4300_COP0_REGISTER_LLADDR\
+   "17"
+#define TUX64_PLATFORM_MIPS_VR4300_COP0_REGISTER_WATCHLO\
+   "18"
+#define TUX64_PLATFORM_MIPS_VR4300_COP0_REGISTER_WATCHHI\
+   "19"
+#define TUX64_PLATFORM_MIPS_VR4300_COP0_REGISTER_XCONTEXT\
+   "20"
+#define TUX64_PLATFORM_MIPS_VR4300_COP0_REGISTER_PARITY_ERROR\
+   "26"
+#define TUX64_PLATFORM_MIPS_VR4300_COP0_REGISTER_CACHE_ERROR\
+   "27"
+#define TUX64_PLATFORM_MIPS_VR4300_COP0_REGISTER_TAGLO\
+   "28"
+#define TUX64_PLATFORM_MIPS_VR4300_COP0_REGISTER_TAGHI\
+   "29"
+#define TUX64_PLATFORM_MIPS_VR4300_COP0_REGISTER_ERROREPC\
+   "30"
 
+#define TUX64_PLATFORM_MIPS_VR4300_COP0_REGISTER_READ(output, identifier)\
+   __asm__ (\
+      "mfc0 %0,$" identifier\
+      : "=r" (output)\
+   );
+#define TUX64_PLATFORM_MIPS_VR4300_COP0_REGISTER_WRITE(value, identifier)\
+   __asm__ volatile (\
+      "mtc0 %0,$" identifier\
+      :: "r" (value)\
+   );
+
+Tux64UInt32
+tux64_platform_mips_vr4300_cop0_register_read_index(void) {
+   Tux64UInt32 output;
+
+   TUX64_PLATFORM_MIPS_VR4300_COP0_REGISTER_READ(
+      output,
+      TUX64_PLATFORM_MIPS_VR4300_COP0_REGISTER_INDEX
+   );
+   
+   return output;
+}
+
+Tux64UInt32
+tux64_platform_mips_vr4300_cop0_register_read_random(void) {
+   Tux64UInt32 output;
+
+   TUX64_PLATFORM_MIPS_VR4300_COP0_REGISTER_READ(
+      output,
+      TUX64_PLATFORM_MIPS_VR4300_COP0_REGISTER_RANDOM
+   );
+   
+   return output;
+}
+
+Tux64UInt32
+tux64_platform_mips_vr4300_cop0_register_read_entrylo0(void) {
+   Tux64UInt32 output;
+
+   TUX64_PLATFORM_MIPS_VR4300_COP0_REGISTER_READ(
+      output,
+      TUX64_PLATFORM_MIPS_VR4300_COP0_REGISTER_ENTRYLO0
+   );
+   
+   return output;
+}
+
+Tux64UInt32
+tux64_platform_mips_vr4300_cop0_register_read_entrylo1(void) {
+   Tux64UInt32 output;
+
+   TUX64_PLATFORM_MIPS_VR4300_COP0_REGISTER_READ(
+      output,
+      TUX64_PLATFORM_MIPS_VR4300_COP0_REGISTER_ENTRYLO1
+   );
+   
+   return output;
+}
+
+Tux64UInt32
+tux64_platform_mips_vr4300_cop0_register_read_context(void) {
+   Tux64UInt32 output;
+
+   TUX64_PLATFORM_MIPS_VR4300_COP0_REGISTER_READ(
+      output,
+      TUX64_PLATFORM_MIPS_VR4300_COP0_REGISTER_CONTEXT
+   );
+   
+   return output;
+}
+
+Tux64UInt32
+tux64_platform_mips_vr4300_cop0_register_read_pagemask(void) {
+   Tux64UInt32 output;
+
+   TUX64_PLATFORM_MIPS_VR4300_COP0_REGISTER_READ(
+      output,
+      TUX64_PLATFORM_MIPS_VR4300_COP0_REGISTER_PAGEMASK
+   );
+   
+   return output;
+}
+
+Tux64UInt32
+tux64_platform_mips_vr4300_cop0_register_read_wired(void) {
+   Tux64UInt32 output;
+
+   TUX64_PLATFORM_MIPS_VR4300_COP0_REGISTER_READ(
+      output,
+      TUX64_PLATFORM_MIPS_VR4300_COP0_REGISTER_WIRED
+   );
+   
+   return output;
+}
+
+Tux64UInt32
+tux64_platform_mips_vr4300_cop0_register_read_badvaddr(void) {
+   Tux64UInt32 output;
+
+   TUX64_PLATFORM_MIPS_VR4300_COP0_REGISTER_READ(
+      output,
+      TUX64_PLATFORM_MIPS_VR4300_COP0_REGISTER_BADVADDR
+   );
+   
+   return output;
+}
+
+Tux64UInt32
+tux64_platform_mips_vr4300_cop0_register_read_count(void) {
+   Tux64UInt32 output;
+
+   TUX64_PLATFORM_MIPS_VR4300_COP0_REGISTER_READ(
+      output,
+      TUX64_PLATFORM_MIPS_VR4300_COP0_REGISTER_COUNT
+   );
+   
+   return output;
+}
+
+Tux64UInt32
+tux64_platform_mips_vr4300_cop0_register_read_entryhi(void) {
+   Tux64UInt32 output;
+
+   TUX64_PLATFORM_MIPS_VR4300_COP0_REGISTER_READ(
+      output,
+      TUX64_PLATFORM_MIPS_VR4300_COP0_REGISTER_ENTRYHI
+   );
+   
+   return output;
+}
+
+Tux64UInt32
+tux64_platform_mips_vr4300_cop0_register_read_compare(void) {
+   Tux64UInt32 output;
+
+   TUX64_PLATFORM_MIPS_VR4300_COP0_REGISTER_READ(
+      output,
+      TUX64_PLATFORM_MIPS_VR4300_COP0_REGISTER_COMPARE
+   );
+   
+   return output;
+}
+
+Tux64UInt32
+tux64_platform_mips_vr4300_cop0_register_read_status(void) {
+   Tux64UInt32 output;
+
+   TUX64_PLATFORM_MIPS_VR4300_COP0_REGISTER_READ(
+      output,
+      TUX64_PLATFORM_MIPS_VR4300_COP0_REGISTER_STATUS
+   );
+   
+   return output;
+}
+
+Tux64UInt32
+tux64_platform_mips_vr4300_cop0_register_read_cause(void) {
+   Tux64UInt32 output;
+
+   TUX64_PLATFORM_MIPS_VR4300_COP0_REGISTER_READ(
+      output,
+      TUX64_PLATFORM_MIPS_VR4300_COP0_REGISTER_CAUSE
+   );
+   
+   return output;
+}
+
+Tux64UInt32
+tux64_platform_mips_vr4300_cop0_register_read_epc(void) {
+   Tux64UInt32 output;
+
+   TUX64_PLATFORM_MIPS_VR4300_COP0_REGISTER_READ(
+      output,
+      TUX64_PLATFORM_MIPS_VR4300_COP0_REGISTER_EPC
+   );
+   
+   return output;
+}
+
+Tux64UInt32
+tux64_platform_mips_vr4300_cop0_register_read_prid(void) {
+   Tux64UInt32 output;
+
+   TUX64_PLATFORM_MIPS_VR4300_COP0_REGISTER_READ(
+      output,
+      TUX64_PLATFORM_MIPS_VR4300_COP0_REGISTER_PRID
+   );
+   
+   return output;
+}
+
+Tux64UInt32
+tux64_platform_mips_vr4300_cop0_register_read_config(void) {
+   Tux64UInt32 output;
+
+   TUX64_PLATFORM_MIPS_VR4300_COP0_REGISTER_READ(
+      output,
+      TUX64_PLATFORM_MIPS_VR4300_COP0_REGISTER_CONFIG
+   );
+   
+   return output;
+}
+
+Tux64UInt32
+tux64_platform_mips_vr4300_cop0_register_read_lladdr(void) {
+   Tux64UInt32 output;
+
+   TUX64_PLATFORM_MIPS_VR4300_COP0_REGISTER_READ(
+      output,
+      TUX64_PLATFORM_MIPS_VR4300_COP0_REGISTER_LLADDR
+   );
+   
+   return output;
+}
+
+Tux64UInt32
+tux64_platform_mips_vr4300_cop0_register_read_watchlo(void) {
+   Tux64UInt32 output;
+
+   TUX64_PLATFORM_MIPS_VR4300_COP0_REGISTER_READ(
+      output,
+      TUX64_PLATFORM_MIPS_VR4300_COP0_REGISTER_WATCHLO
+   );
+   
+   return output;
+}
+
+Tux64UInt32
+tux64_platform_mips_vr4300_cop0_register_read_watchhi(void) {
+   Tux64UInt32 output;
+
+   TUX64_PLATFORM_MIPS_VR4300_COP0_REGISTER_READ(
+      output,
+      TUX64_PLATFORM_MIPS_VR4300_COP0_REGISTER_WATCHHI
+   );
+   
+   return output;
+}
+
+Tux64UInt32
+tux64_platform_mips_vr4300_cop0_register_read_xcontext(void) {
+   Tux64UInt32 output;
+
+   TUX64_PLATFORM_MIPS_VR4300_COP0_REGISTER_READ(
+      output,
+      TUX64_PLATFORM_MIPS_VR4300_COP0_REGISTER_XCONTEXT
+   );
+   
+   return output;
+}
+
+Tux64UInt32
+tux64_platform_mips_vr4300_cop0_register_read_parity_error(void) {
+   Tux64UInt32 output;
+
+   TUX64_PLATFORM_MIPS_VR4300_COP0_REGISTER_READ(
+      output,
+      TUX64_PLATFORM_MIPS_VR4300_COP0_REGISTER_PARITY_ERROR
+   );
+   
+   return output;
+}
+
+Tux64UInt32
+tux64_platform_mips_vr4300_cop0_register_read_cache_error(void) {
+   Tux64UInt32 output;
+
+   TUX64_PLATFORM_MIPS_VR4300_COP0_REGISTER_READ(
+      output,
+      TUX64_PLATFORM_MIPS_VR4300_COP0_REGISTER_CACHE_ERROR
+   );
+   
+   return output;
+}
+
+Tux64UInt32
+tux64_platform_mips_vr4300_cop0_register_read_taglo(void) {
+   Tux64UInt32 output;
+
+   TUX64_PLATFORM_MIPS_VR4300_COP0_REGISTER_READ(
+      output,
+      TUX64_PLATFORM_MIPS_VR4300_COP0_REGISTER_TAGLO
+   );
+   
+   return output;
+}
+
+Tux64UInt32
+tux64_platform_mips_vr4300_cop0_register_read_taghi(void) {
+   Tux64UInt32 output;
+
+   TUX64_PLATFORM_MIPS_VR4300_COP0_REGISTER_READ(
+      output,
+      TUX64_PLATFORM_MIPS_VR4300_COP0_REGISTER_TAGHI
+   );
+   
+   return output;
+}
+
+Tux64UInt32
+tux64_platform_mips_vr4300_cop0_register_read_errorepc(void) {
+   Tux64UInt32 output;
+
+   TUX64_PLATFORM_MIPS_VR4300_COP0_REGISTER_READ(
+      output,
+      TUX64_PLATFORM_MIPS_VR4300_COP0_REGISTER_ERROREPC
+   );
+   
+   return output;
+}
+
+void
+tux64_platform_mips_vr4300_cop0_register_write_index(
+   Tux64UInt32 value
+) {
+   TUX64_PLATFORM_MIPS_VR4300_COP0_REGISTER_WRITE(
+      value,
+      TUX64_PLATFORM_MIPS_VR4300_COP0_REGISTER_INDEX
+   );
+
+   return;
+}
+
+void
+tux64_platform_mips_vr4300_cop0_register_write_random(
+   Tux64UInt32 value
+) {
+   TUX64_PLATFORM_MIPS_VR4300_COP0_REGISTER_WRITE(
+      value,
+      TUX64_PLATFORM_MIPS_VR4300_COP0_REGISTER_RANDOM
+   );
+
+   return;
+}
+
+void
+tux64_platform_mips_vr4300_cop0_register_write_entrylo0(
+   Tux64UInt32 value
+) {
+   TUX64_PLATFORM_MIPS_VR4300_COP0_REGISTER_WRITE(
+      value,
+      TUX64_PLATFORM_MIPS_VR4300_COP0_REGISTER_ENTRYLO0
+   );
+
+   return;
+}
+
+void
+tux64_platform_mips_vr4300_cop0_register_write_entrylo1(
+   Tux64UInt32 value
+) {
+   TUX64_PLATFORM_MIPS_VR4300_COP0_REGISTER_WRITE(
+      value,
+      TUX64_PLATFORM_MIPS_VR4300_COP0_REGISTER_ENTRYLO1
+   );
+
+   return;
+}
+
+void
+tux64_platform_mips_vr4300_cop0_register_write_context(
+   Tux64UInt32 value
+) {
+   TUX64_PLATFORM_MIPS_VR4300_COP0_REGISTER_WRITE(
+      value,
+      TUX64_PLATFORM_MIPS_VR4300_COP0_REGISTER_CONTEXT
+   );
+
+   return;
+}
+
+void
+tux64_platform_mips_vr4300_cop0_register_write_pagemask(
+   Tux64UInt32 value
+) {
+   TUX64_PLATFORM_MIPS_VR4300_COP0_REGISTER_WRITE(
+      value,
+      TUX64_PLATFORM_MIPS_VR4300_COP0_REGISTER_PAGEMASK
+   );
+
+   return;
+}
+
+void
+tux64_platform_mips_vr4300_cop0_register_write_wired(
+   Tux64UInt32 value
+) {
+   TUX64_PLATFORM_MIPS_VR4300_COP0_REGISTER_WRITE(
+      value,
+      TUX64_PLATFORM_MIPS_VR4300_COP0_REGISTER_WIRED
+   );
+
+   return;
+}
+
+void
+tux64_platform_mips_vr4300_cop0_register_write_badvaddr(
+   Tux64UInt32 value
+) {
+   TUX64_PLATFORM_MIPS_VR4300_COP0_REGISTER_WRITE(
+      value,
+      TUX64_PLATFORM_MIPS_VR4300_COP0_REGISTER_BADVADDR
+   );
+
+   return;
+}
+
+void
+tux64_platform_mips_vr4300_cop0_register_write_count(
+   Tux64UInt32 value
+) {
+   TUX64_PLATFORM_MIPS_VR4300_COP0_REGISTER_WRITE(
+      value,
+      TUX64_PLATFORM_MIPS_VR4300_COP0_REGISTER_COUNT
+   );
+
+   return;
+}
+
+void
+tux64_platform_mips_vr4300_cop0_register_write_entryhi(
+   Tux64UInt32 value
+) {
+   TUX64_PLATFORM_MIPS_VR4300_COP0_REGISTER_WRITE(
+      value,
+      TUX64_PLATFORM_MIPS_VR4300_COP0_REGISTER_ENTRYHI
+   );
+
+   return;
+}
+
+void
+tux64_platform_mips_vr4300_cop0_register_write_compare(
+   Tux64UInt32 value
+) {
+   TUX64_PLATFORM_MIPS_VR4300_COP0_REGISTER_WRITE(
+      value,
+      TUX64_PLATFORM_MIPS_VR4300_COP0_REGISTER_COMPARE
+   );
+
+   return;
+}
+
+void
+tux64_platform_mips_vr4300_cop0_register_write_status(
+   Tux64UInt32 value
+) {
+   TUX64_PLATFORM_MIPS_VR4300_COP0_REGISTER_WRITE(
+      value,
+      TUX64_PLATFORM_MIPS_VR4300_COP0_REGISTER_STATUS
+   );
+
+   return;
+}
+
+void
+tux64_platform_mips_vr4300_cop0_register_write_cause(
+   Tux64UInt32 value
+) {
+   TUX64_PLATFORM_MIPS_VR4300_COP0_REGISTER_WRITE(
+      value,
+      TUX64_PLATFORM_MIPS_VR4300_COP0_REGISTER_CAUSE
+   );
+
+   return;
+}
+
+void
+tux64_platform_mips_vr4300_cop0_register_write_epc(
+   Tux64UInt32 value
+) {
+   TUX64_PLATFORM_MIPS_VR4300_COP0_REGISTER_WRITE(
+      value,
+      TUX64_PLATFORM_MIPS_VR4300_COP0_REGISTER_EPC
+   );
+
+   return;
+}
+
+void
+tux64_platform_mips_vr4300_cop0_register_write_prid(
+   Tux64UInt32 value
+) {
+   TUX64_PLATFORM_MIPS_VR4300_COP0_REGISTER_WRITE(
+      value,
+      TUX64_PLATFORM_MIPS_VR4300_COP0_REGISTER_PRID
+   );
+
+   return;
+}
+
+void
+tux64_platform_mips_vr4300_cop0_register_write_config(
+   Tux64UInt32 value
+) {
+   TUX64_PLATFORM_MIPS_VR4300_COP0_REGISTER_WRITE(
+      value,
+      TUX64_PLATFORM_MIPS_VR4300_COP0_REGISTER_CONFIG
+   );
+
+   return;
+}
+
+void
+tux64_platform_mips_vr4300_cop0_register_write_lladdr(
+   Tux64UInt32 value
+) {
+   TUX64_PLATFORM_MIPS_VR4300_COP0_REGISTER_WRITE(
+      value,
+      TUX64_PLATFORM_MIPS_VR4300_COP0_REGISTER_LLADDR
+   );
+
+   return;
+}
+
+void
+tux64_platform_mips_vr4300_cop0_register_write_watchlo(
+   Tux64UInt32 value
+) {
+   TUX64_PLATFORM_MIPS_VR4300_COP0_REGISTER_WRITE(
+      value,
+      TUX64_PLATFORM_MIPS_VR4300_COP0_REGISTER_WATCHLO
+   );
+
+   return;
+}
+
+void
+tux64_platform_mips_vr4300_cop0_register_write_watchhi(
+   Tux64UInt32 value
+) {
+   TUX64_PLATFORM_MIPS_VR4300_COP0_REGISTER_WRITE(
+      value,
+      TUX64_PLATFORM_MIPS_VR4300_COP0_REGISTER_WATCHHI
+   );
+
+   return;
+}
+
+void
+tux64_platform_mips_vr4300_cop0_register_write_xcontext(
+   Tux64UInt32 value
+) {
+   TUX64_PLATFORM_MIPS_VR4300_COP0_REGISTER_WRITE(
+      value,
+      TUX64_PLATFORM_MIPS_VR4300_COP0_REGISTER_XCONTEXT
+   );
+
+   return;
+}
+
+void
+tux64_platform_mips_vr4300_cop0_register_write_parity_error(
+   Tux64UInt32 value
+) {
+   TUX64_PLATFORM_MIPS_VR4300_COP0_REGISTER_WRITE(
+      value,
+      TUX64_PLATFORM_MIPS_VR4300_COP0_REGISTER_PARITY_ERROR
+   );
+
+   return;
+}
+
+void
+tux64_platform_mips_vr4300_cop0_register_write_cache_error(
+   Tux64UInt32 value
+) {
+   TUX64_PLATFORM_MIPS_VR4300_COP0_REGISTER_WRITE(
+      value,
+      TUX64_PLATFORM_MIPS_VR4300_COP0_REGISTER_CACHE_ERROR
+   );
+
+   return;
+}
+
+void
+tux64_platform_mips_vr4300_cop0_register_write_taglo(
+   Tux64UInt32 value
+) {
+   TUX64_PLATFORM_MIPS_VR4300_COP0_REGISTER_WRITE(
+      value,
+      TUX64_PLATFORM_MIPS_VR4300_COP0_REGISTER_TAGLO
+   );
+
+   return;
+}
+
+void
+tux64_platform_mips_vr4300_cop0_register_write_taghi(
+   Tux64UInt32 value
+) {
+   TUX64_PLATFORM_MIPS_VR4300_COP0_REGISTER_WRITE(
+      value,
+      TUX64_PLATFORM_MIPS_VR4300_COP0_REGISTER_TAGHI
+   );
+
+   return;
+}
+
+void
+tux64_platform_mips_vr4300_cop0_register_write_errorepc(
+   Tux64UInt32 value
+) {
+   TUX64_PLATFORM_MIPS_VR4300_COP0_REGISTER_WRITE(
+      value,
+      TUX64_PLATFORM_MIPS_VR4300_COP0_REGISTER_ERROREPC
+   );
+
+   return;
+}
 
 /*----------------------------------------------------------------------------*/
 #endif /* TUX64_PLATFORM_MIPS_VR4300_ENABLE */
--- lib/src/tux64/platform/mips/vr4300/cop0.h
+++ lib/src/tux64/platform/mips/vr4300/cop0.h
@@ -16,7 +16,160 @@
 #if TUX64_PLATFORM_MIPS_VR4300_ENABLE
 /*----------------------------------------------------------------------------*/
 
-
+/*----------------------------------------------------------------------------*/
+/* Reads directly from or writes directly to various co-processor 0           */
+/* registers.                                                                 */
+/*----------------------------------------------------------------------------*/
+Tux64UInt32
+tux64_platform_mips_vr4300_cop0_register_read_index(void);
+Tux64UInt32
+tux64_platform_mips_vr4300_cop0_register_read_random(void);
+Tux64UInt32
+tux64_platform_mips_vr4300_cop0_register_read_entrylo0(void);
+Tux64UInt32
+tux64_platform_mips_vr4300_cop0_register_read_entrylo1(void);
+Tux64UInt32
+tux64_platform_mips_vr4300_cop0_register_read_context(void);
+Tux64UInt32
+tux64_platform_mips_vr4300_cop0_register_read_pagemask(void);
+Tux64UInt32
+tux64_platform_mips_vr4300_cop0_register_read_wired(void);
+Tux64UInt32
+tux64_platform_mips_vr4300_cop0_register_read_badvaddr(void);
+Tux64UInt32
+tux64_platform_mips_vr4300_cop0_register_read_count(void);
+Tux64UInt32
+tux64_platform_mips_vr4300_cop0_register_read_entryhi(void);
+Tux64UInt32
+tux64_platform_mips_vr4300_cop0_register_read_compare(void);
+Tux64UInt32
+tux64_platform_mips_vr4300_cop0_register_read_status(void);
+Tux64UInt32
+tux64_platform_mips_vr4300_cop0_register_read_cause(void);
+Tux64UInt32
+tux64_platform_mips_vr4300_cop0_register_read_epc(void);
+Tux64UInt32
+tux64_platform_mips_vr4300_cop0_register_read_prid(void);
+Tux64UInt32
+tux64_platform_mips_vr4300_cop0_register_read_config(void);
+Tux64UInt32
+tux64_platform_mips_vr4300_cop0_register_read_lladdr(void);
+Tux64UInt32
+tux64_platform_mips_vr4300_cop0_register_read_watchlo(void);
+Tux64UInt32
+tux64_platform_mips_vr4300_cop0_register_read_watchhi(void);
+Tux64UInt32
+tux64_platform_mips_vr4300_cop0_register_read_xcontext(void);
+Tux64UInt32
+tux64_platform_mips_vr4300_cop0_register_read_parity_error(void);
+Tux64UInt32
+tux64_platform_mips_vr4300_cop0_register_read_cache_error(void);
+Tux64UInt32
+tux64_platform_mips_vr4300_cop0_register_read_taglo(void);
+Tux64UInt32
+tux64_platform_mips_vr4300_cop0_register_read_taghi(void);
+Tux64UInt32
+tux64_platform_mips_vr4300_cop0_register_read_errorepc(void);
+void
+tux64_platform_mips_vr4300_cop0_register_write_index(
+   Tux64UInt32 value
+);
+void
+tux64_platform_mips_vr4300_cop0_register_write_random(
+   Tux64UInt32 value
+);
+void
+tux64_platform_mips_vr4300_cop0_register_write_entrylo0(
+   Tux64UInt32 value
+);
+void
+tux64_platform_mips_vr4300_cop0_register_write_entrylo1(
+   Tux64UInt32 value
+);
+void
+tux64_platform_mips_vr4300_cop0_register_write_context(
+   Tux64UInt32 value
+);
+void
+tux64_platform_mips_vr4300_cop0_register_write_pagemask(
+   Tux64UInt32 value
+);
+void
+tux64_platform_mips_vr4300_cop0_register_write_wired(
+   Tux64UInt32 value
+);
+void
+tux64_platform_mips_vr4300_cop0_register_write_badvaddr(
+   Tux64UInt32 value
+);
+void
+tux64_platform_mips_vr4300_cop0_register_write_count(
+   Tux64UInt32 value
+);
+void
+tux64_platform_mips_vr4300_cop0_register_write_entryhi(
+   Tux64UInt32 value
+);
+void
+tux64_platform_mips_vr4300_cop0_register_write_compare(
+   Tux64UInt32 value
+);
+void
+tux64_platform_mips_vr4300_cop0_register_write_status(
+   Tux64UInt32 value
+);
+void
+tux64_platform_mips_vr4300_cop0_register_write_cause(
+   Tux64UInt32 value
+);
+void
+tux64_platform_mips_vr4300_cop0_register_write_epc(
+   Tux64UInt32 value
+);
+void
+tux64_platform_mips_vr4300_cop0_register_write_prid(
+   Tux64UInt32 value
+);
+void
+tux64_platform_mips_vr4300_cop0_register_write_config(
+   Tux64UInt32 value
+);
+void
+tux64_platform_mips_vr4300_cop0_register_write_lladdr(
+   Tux64UInt32 value
+);
+void
+tux64_platform_mips_vr4300_cop0_register_write_watchlo(
+   Tux64UInt32 value
+);
+void
+tux64_platform_mips_vr4300_cop0_register_write_watchhi(
+   Tux64UInt32 value
+);
+void
+tux64_platform_mips_vr4300_cop0_register_write_xcontext(
+   Tux64UInt32 value
+);
+void
+tux64_platform_mips_vr4300_cop0_register_write_parity_error(
+   Tux64UInt32 value
+);
+void
+tux64_platform_mips_vr4300_cop0_register_write_cache_error(
+   Tux64UInt32 value
+);
+void
+tux64_platform_mips_vr4300_cop0_register_write_taglo(
+   Tux64UInt32 value
+);
+void
+tux64_platform_mips_vr4300_cop0_register_write_taghi(
+   Tux64UInt32 value
+);
+void
+tux64_platform_mips_vr4300_cop0_register_write_errorepc(
+   Tux64UInt32 value
+);
 
 /*----------------------------------------------------------------------------*/
 #endif /* TUX64_PLATFORM_MIPS_VR4300_ENABLE */
