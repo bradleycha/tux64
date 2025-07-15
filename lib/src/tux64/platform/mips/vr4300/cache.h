8992ea97655c37f154adc8cbf95f5581808fabc8
Chase Bradley <bradleycha@proton.me>
2025-07-14 23:16:05 -0400

    tux64-lib: create cache constants for VR4300

--- lib/src/tux64/platform/mips/vr4300/cache.h
+++ lib/src/tux64/platform/mips/vr4300/cache.h
@@ -33,6 +33,42 @@
    (512u)
 
 /*----------------------------------------------------------------------------*/
+/* Cache operation target constants, for use with the 'cache' instruction.    */
+/*----------------------------------------------------------------------------*/
+#define TUX64_PLATFORM_MIPS_VR4300_CACHE_OPERATION_TARGET_INSTRUCTION\
+   (0x00u)
+#define TUX64_PLATFORM_MIPS_VR4300_CACHE_OPERATION_TARGET_DATA\
+   (0x01u)
+
+/*----------------------------------------------------------------------------*/
+/* Cache operation type constants, for use with the 'cache' instruction.      */
+/*----------------------------------------------------------------------------*/
+#define TUX64_PLATFORM_MIPS_VR4300_CACHE_OPERATION_TYPE_INDEX_INVALIDATE\
+   (0x00u)
+#define TUX64_PLATFORM_MIPS_VR4300_CACHE_OPERATION_TYPE_INDEX_WRITE_BACK_INVALIDATE\
+   (0x00u)
+#define TUX64_PLATFORM_MIPS_VR4300_CACHE_OPERATION_TYPE_INDEX_LOAD_TAG\
+   (0x01u)
+#define TUX64_PLATFORM_MIPS_VR4300_CACHE_OPERATION_TYPE_INDEX_STORE_TAG\
+   (0x02u)
+#define TUX64_PLATFORM_MIPS_VR4300_CACHE_OPERATION_TYPE_CREATE_DIRTY_EXCLUSIVE\
+   (0x03u)
+#define TUX64_PLATFORM_MIPS_VR4300_CACHE_OPERATION_TYPE_HIT_INVALIDATE\
+   (0x04u)
+#define TUX64_PLATFORM_MIPS_VR4300_CACHE_OPERATION_TYPE_HIT_WRITE_BACK_INVALIDATE\
+   (0x05u)
+#define TUX64_PLATFORM_MIPS_VR4300_CACHE_OPERATION_TYPE_FILL\
+   (0x05u)
+#define TUX64_PLATFORM_MIPS_VR4300_CACHE_OPERATION_TYPE_HIT_WRITE_BACK\
+   (0x06u)
+
+/*----------------------------------------------------------------------------*/
+/* Constructs a cache operation literal for use with the 'cache' instruction. */
+/*----------------------------------------------------------------------------*/
+#define TUX64_PLATFORM_MIPS_VR4300_CACHE_OPERATION(target, operation)\
+   ((target) | ((operation) << 2u))
+
+/*----------------------------------------------------------------------------*/
 /* The raw cache instruction.  This should be avoided at all costs, using     */
 /* wrapper functions if possible.  'address' is the address to perform the    */
 /* cache instruction on, and 'operation' is the constant describing the cache */
