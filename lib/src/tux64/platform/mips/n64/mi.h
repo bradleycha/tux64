570bacc7652b391b727fbdd870aaa1c6d848df05
Chase Bradley <bradleycha@proton.me>
2025-07-22 21:09:45 -0400

    tux64-lib: implement correct bit flags for MI_MASK

--- lib/src/tux64/platform/mips/n64/mi.h
+++ lib/src/tux64/platform/mips/n64/mi.h
@@ -64,18 +64,30 @@
    (1u << 5u)
 
 /* MI_MASK bits */
-#define TUX64_PLATFORM_MIPS_N64_MI_MASK_BIT_SP\
-   (1u << 0u)
-#define TUX64_PLATFORM_MIPS_N64_MI_MASK_BIT_SI\
-   (1u << 1u)
-#define TUX64_PLATFORM_MIPS_N64_MI_MASK_BIT_AI\
-   (1u << 2u)
-#define TUX64_PLATFORM_MIPS_N64_MI_MASK_BIT_VI\
-   (1u << 3u)
-#define TUX64_PLATFORM_MIPS_N64_MI_MASK_BIT_PI\
-   (1u << 4u)
-#define TUX64_PLATFORM_MIPS_N64_MI_MASK_BIT_DP\
-   (1u << 5u)
+#define TUX64_PLATFORM_MIPS_N64_MI_MASK_BIT_SET_SP\
+   (1u <<  1u)
+#define TUX64_PLATFORM_MIPS_N64_MI_MASK_BIT_SET_SI\
+   (1u <<  3u)
+#define TUX64_PLATFORM_MIPS_N64_MI_MASK_BIT_SET_AI\
+   (1u <<  5u)
+#define TUX64_PLATFORM_MIPS_N64_MI_MASK_BIT_SET_VI\
+   (1u <<  7u)
+#define TUX64_PLATFORM_MIPS_N64_MI_MASK_BIT_SET_PI\
+   (1u <<  9u)
+#define TUX64_PLATFORM_MIPS_N64_MI_MASK_BIT_SET_DP\
+   (1u << 11u)
+#define TUX64_PLATFORM_MIPS_N64_MI_MASK_BIT_CLEAR_SP\
+   (1u <<  0u)
+#define TUX64_PLATFORM_MIPS_N64_MI_MASK_BIT_CLEAR_SI\
+   (1u <<  2u)
+#define TUX64_PLATFORM_MIPS_N64_MI_MASK_BIT_CLEAR_AI\
+   (1u <<  4u)
+#define TUX64_PLATFORM_MIPS_N64_MI_MASK_BIT_CLEAR_VI\
+   (1u <<  6u)
+#define TUX64_PLATFORM_MIPS_N64_MI_MASK_BIT_CLEAR_PI\
+   (1u <<  8u)
+#define TUX64_PLATFORM_MIPS_N64_MI_MASK_BIT_CLEAR_DP\
+   (1u << 10u)
 
 /*----------------------------------------------------------------------------*/
 #endif /* _TUX64_PLATFORM_MIPS_N64_MI_H */
