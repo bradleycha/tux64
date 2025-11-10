189322b1dcece21963adb407948fbb94a0f42c4d
Chase Bradley <bradleycha@proton.me>
2025-11-10 14:02:59 -0500

    tux64-boot: create abstraction for issuing rsp dma requests

---
+++ boot/src/tux64-boot/stage1/rsp.c
@@ -0,0 +1,81 @@
+/*----------------------------------------------------------------------------*/
+/*                          Copyright (C) Tux64 2025                          */
+/*                    https://github.com/bradleycha/tux64                     */
+/*----------------------------------------------------------------------------*/
+/* boot/src/tux64-boot/stage1/rsp.c - Implementations for controlling the     */
+/*    RSP.                                                                    */
+/*----------------------------------------------------------------------------*/
+
+#include "tux64-boot/tux64-boot.h"
+#include "tux64-boot/stage1/rsp.h"
+
+#include <tux64/platform/mips/n64/mmio.h>
+#include "tux64-boot/stage1/idle.h"
+
+static Tux64UInt32
+tux64_boot_stage1_rsp_dma_encode_length(
+   Tux64UInt16 row_bytes_copy,
+   Tux64UInt16 row_bytes_skip,
+   Tux64UInt8 row_count
+) {
+   Tux64UInt32 retn;
+
+   retn = TUX64_LITERAL_UINT32(0u);
+   retn = retn | (Tux64UInt32)(row_bytes_copy   << TUX64_LITERAL_UINT8( 0u));
+   retn = retn | (Tux64UInt32)(row_count        << TUX64_LITERAL_UINT8(12u));
+   retn = retn | (Tux64UInt32)(row_bytes_skip   << TUX64_LITERAL_UINT8(20u));
+
+   return retn;
+}
+
+void
+tux64_boot_stage1_rsp_dma_start(
+   const struct Tux64BootStage1RspDmaTransfer * transfer,
+   enum Tux64BootStage1RspDmaDestination destination
+) {
+   volatile Tux64UInt32 * reg;
+   Tux64UInt32 length;
+
+   switch (destination) {
+      case TUX64_BOOT_STAGE1_RSP_DMA_DESTINATION_RSP_MEMORY:
+         reg = &tux64_platform_mips_n64_mmio_registers_sp.dma_rdlen;
+         break;
+      case TUX64_BOOT_STAGE1_RSP_DMA_DESTINATION_RDRAM:
+         reg = &tux64_platform_mips_n64_mmio_registers_sp.dma_wrlen;
+         break;
+      default:
+         TUX64_UNREACHABLE;
+   }
+
+   length = tux64_boot_stage1_rsp_dma_encode_length(
+      transfer->row_bytes_copy,
+      transfer->row_bytes_skip,
+      transfer->row_count
+   );
+
+   /* upper bits which encode the virtual address are ignored, so we can just */
+   /* directly write these. */
+   tux64_platform_mips_n64_mmio_registers_sp.dma_spaddr  = transfer->addr_rsp_mem;
+   tux64_platform_mips_n64_mmio_registers_sp.dma_ramaddr = transfer->addr_rdram;
+
+   /* writing to this register starts the DMA operations, or queues it up if */
+   /* there is already a DMA operation in progress. */
+   *reg = length;
+
+   return;
+}
+
+void
+tux64_boot_stage1_rsp_dma_wait_idle(void) {
+   Tux64UInt32 dma_busy;
+
+   /* wait for the DMA_BUSY bit in SP_STATUS to be clear in a spinlock */
+   tux64_boot_stage1_idle_enter();
+   do {
+      dma_busy = tux64_platform_mips_n64_mmio_registers_sp.dma_busy;
+   } while (dma_busy != TUX64_LITERAL_UINT32(0u));
+   tux64_boot_stage1_idle_exit();
+
+   return;
+}
+
---
+++ boot/src/tux64-boot/stage1/rsp.h
@@ -0,0 +1,71 @@
+/*----------------------------------------------------------------------------*/
+/*                          Copyright (C) Tux64 2025                          */
+/*                    https://github.com/bradleycha/tux64                     */
+/*----------------------------------------------------------------------------*/
+/* boot/src/tux64-boot/stage1/rsp.h - Header for controlling the RSP.         */
+/*----------------------------------------------------------------------------*/
+
+#ifndef _TUX64_BOOT_STAGE1_RSP_H
+#define _TUX64_BOOT_STAGE1_RSP_H
+/*----------------------------------------------------------------------------*/
+
+#include "tux64-boot/tux64-boot.h"
+
+/*----------------------------------------------------------------------------*/
+/* Describes an RSP DMA transfer operation.                                   */
+/*----------------------------------------------------------------------------*/
+/* addr_rsp_mem -                                                             */
+/*    The address in RSP memory to start the transfer at.  This must be       */
+/*    aligned to 8 bytes.  This can either be a physical, uncached virtual,   */
+/*    or cached virtual address.                                              */
+/*                                                                            */
+/* addr_rdram -                                                               */
+/*    The physical address in RDRAM to start the transfer at.  This must be   */
+/*    aligned to 8 bytes.  This can either be a physical, uncached virtual,   */
+/*    or cached virtual address.                                              */
+/*                                                                            */
+/* row_bytes_copy -                                                           */
+/*    The amount of bytes per row to copy, starting at the beginning of the   */
+/*    row.  This has a maximum value of (2^12 - 1).                           */
+/*                                                                            */
+/* row_bytes_skip -                                                           */
+/*    The amount of bytes per row to skip over and get to the next row,       */
+/*    starting after the copied bytes.  This has a maximum value of           */
+/*    (2^12 - 1).                                                             */
+/*                                                                            */
+/* row_count -                                                                */
+/*    The number of rows to copy, minus one.  For example, to copy a single   */
+/*    row, this would be '0'.  To copy 16 lines, this would be '15'.          */
+/*----------------------------------------------------------------------------*/
+struct Tux64BootStage1RspDmaTransfer {
+   Tux64UInt32 addr_rsp_mem;
+   Tux64UInt32 addr_rdram;
+   Tux64UInt16 row_bytes_copy;
+   Tux64UInt16 row_bytes_skip;
+   Tux64UInt8  row_count;
+};
+
+enum Tux64BootStage1RspDmaDestination {
+   TUX64_BOOT_STAGE1_RSP_DMA_DESTINATION_RSP_MEMORY,
+   TUX64_BOOT_STAGE1_RSP_DMA_DESTINATION_RDRAM
+};
+
+/*----------------------------------------------------------------------------*/
+/* Starts an RSP DMA transfer asynchronously.  The RSP must not be in the     */
+/* middle of an ongoing DMA transfer when this function is called.            */
+/*----------------------------------------------------------------------------*/
+void
+tux64_boot_stage1_rsp_dma_start(
+   const struct Tux64BootStage1RspDmaTransfer * transfer,
+   enum Tux64BootStage1RspDmaDestination destination
+);
+
+/*----------------------------------------------------------------------------*/
+/* Waits for the RSP to complete its last DMA transfer.                       */
+/*----------------------------------------------------------------------------*/
+void
+tux64_boot_stage1_rsp_dma_wait_idle(void);
+
+/*----------------------------------------------------------------------------*/
+#endif /* _TUX64_BOOT_STAGE1_RSP_H */
+
