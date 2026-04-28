a8ad46d5a161826a05867b2e3bd6142421f67392
Chase Bradley <bradleycha@proton.me>
2026-04-28 04:05:22 -0400

    tux64-lib: add kernel memory usage to boot header

--- lib/src/tux64/platform/mips/n64/boot.h
+++ lib/src/tux64/platform/mips/n64/boot.h
@@ -50,8 +50,14 @@ struct Tux64PlatformMipsN64BootHeaderFileBootloader {
    struct Tux64PlatformMipsN64BootHeaderFile stage2;
 };
 
+struct Tux64PlatformMipsN64BootHeaderFileKernelImage {
+   Tux64UInt32 checksum;
+   Tux64UInt32 length_words;
+   Tux64UInt32 memory_words;
+};
+
 struct Tux64PlatformMipsN64BootHeaderFileKernel {
-   struct Tux64PlatformMipsN64BootHeaderFile image;
+   struct Tux64PlatformMipsN64BootHeaderFileKernelImage image;
    Tux64UInt32 addr_load;
    Tux64UInt32 addr_entry;
    Tux64UInt32 alignment;
