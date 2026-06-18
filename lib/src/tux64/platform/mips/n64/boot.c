9e871860b2bb251d78651642b38212b0181ab0fa
Chase Bradley <bradleycha@proton.me>
2026-06-18 02:59:45 -0400

    tux64-lib: add static assertions for manually-defined constants in platform/mips/n64/boot.h

---
+++ lib/src/tux64/platform/mips/n64/boot.c
@@ -0,0 +1,15 @@
+/*----------------------------------------------------------------------------*/
+/*                          Copyright (C) Tux64 2026                          */
+/*                    https://github.com/bradleycha/tux64                     */
+/*----------------------------------------------------------------------------*/
+/* lib/src/tux64/platform/mips/n64/boot.c - Common function implementations   */
+/*    used by the bootloader.                                                 */
+/*----------------------------------------------------------------------------*/
+
+#include "tux64/tux64.h"
+#include "tux64/platform/mips/n64/boot.h"
+
+/* until we add any helper functions, this only exists to make sure our */
+/* static assertions get tripped during the library's build.  otherwise, we */
+/* could have broken code sneak into git commits before being caught. */
+
