94af3b1d41d26cf0ce6aff691bdc4d835cb18725
Chase Bradley <bradleycha@proton.me>
2026-06-20 01:06:04 -0400

    tux64-boot: add definition for location of stage-1 stack in memory to stage1.h

--- boot/src/tux64-boot/stage1/stack.h
+++ boot/src/tux64-boot/stage1/stack.h
@@ -10,13 +10,19 @@
 /*----------------------------------------------------------------------------*/
 
 #include "tux64-boot/tux64-boot.h"
+#include <tux64/platform/mips/n64/memory-map.h>
 
 /*----------------------------------------------------------------------------*/
 /* The size of the stack, in bytes.                                           */
 /*----------------------------------------------------------------------------*/
-
 #define TUX64_BOOT_STAGE1_STACK_BYTES\
    (0x00001000u) /* 4KiB stack, should be plenty */
+
+/*----------------------------------------------------------------------------*/
+/* The location of the start of the stack in memory.                          */
+/*----------------------------------------------------------------------------*/
+#define TUX64_BOOT_STAGE1_STACK_ADDRESS\
+   TUX64_PLATFORM_MIPS_N64_MEMORY_MAP_ADDRESS_RDRAM_CACHED
 
 /*----------------------------------------------------------------------------*/
 #endif /* _TUX64_BOOT_STAGE1_H */
