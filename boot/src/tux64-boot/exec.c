ccd30f6eb8be22c61da6f055c18d44c7af7f0943
Chase Bradley <bradleycha@proton.me>
2026-06-21 18:25:48 -0400

    tux64-boot: rework exec.c to not break in debug builds

--- boot/src/tux64-boot/exec.c
+++ boot/src/tux64-boot/exec.c
@@ -14,33 +14,19 @@
 #include "tux64-boot/load.h"
 #include "tux64-boot/stage2/stack.h"
 
-typedef void (*Tux64BootExecStartFunction)();
-
-/* like in the stage-1 interrupt handler, gcc will complain if we directly */
-/* pointer cast. */
-union Tux64BootExecStartPtr {
-   Tux64BootExecStartFunction function;
-   const void * data;
-};
-
 void
 tux64_boot_exec_kernel(
    const void * entrypoint,
    const struct Tux64BootExecKernelArguments * arguments
 ) {
-   /* we're manually specifying the registers instead of adding function */
-   /* arguments to ensure a stable ABI. */
-   union Tux64BootExecStartPtr start; 
    Tux64UInt32 fw_arg0_u32;
    Tux64UInt32 fw_arg1_u32;
    Tux64UInt32 fw_arg2_u32;
    Tux64UInt32 fw_arg3_u32;
-   register unsigned long fw_arg0 __asm__("$a0");
-   register unsigned long fw_arg1 __asm__("$a1");
-   register unsigned long fw_arg2 __asm__("$a2");
-   register unsigned long fw_arg3 __asm__("$a3");
-
-   start.data  = entrypoint;
+   unsigned long fw_arg0;
+   unsigned long fw_arg1;
+   unsigned long fw_arg2;
+   unsigned long fw_arg3;
 
    fw_arg0_u32 = (Tux64UIntPtr)arguments;
    fw_arg1_u32 = TUX64_LITERAL_UINT32(0u);
@@ -53,36 +39,47 @@ tux64_boot_exec_kernel(
    fw_arg2 = tux64_endian_convert_uint32(fw_arg2_u32, TUX64_ENDIAN_FORMAT_BIG);
    fw_arg3 = tux64_endian_convert_uint32(fw_arg3_u32, TUX64_ENDIAN_FORMAT_BIG);
 
-   TUX64_EXPLICIT_DEPENDENCY(fw_arg0);
-   TUX64_EXPLICIT_DEPENDENCY(fw_arg1);
-   TUX64_EXPLICIT_DEPENDENCY(fw_arg2);
-   TUX64_EXPLICIT_DEPENDENCY(fw_arg3);
-   start.function();
+   /* need to write asm explicitly so compiler doesn't bugger up the jump. */
+   /* also need .set noreorder so assembler doesn't insert useless nops. */
+   __asm__ volatile (
+      ".set noreorder\n"
+      "jr %0\n"
+      "nop\n"
+      :: "r"      (entrypoint),
+         "{a0}"   (fw_arg0),
+         "{a1}"   (fw_arg1),
+         "{a2}"   (fw_arg2),
+         "{a3}"   (fw_arg3)
+   );
    TUX64_UNREACHABLE;
 }
+
+/* needs to be a macro so it's evaluated as a constant for use with the "K" */
+/* assembler template constraint. */
+#define TUX64_BOOT_EXEC_STAGE2_STACK_POINTER \
+   (TUX64_BOOT_STAGE2_STACK_ADDRESS + TUX64_BOOT_STAGE2_STACK_BYTES)
 
 void
 tux64_boot_exec_stage2(
    Tux64UInt32 memory_total,
    Tux64BootLoadStatus load_status
 ) {
-   /* same comment as above about the stable ABI.  also, this time, we need */
-   /* to manually set the stack pointer. */
-   union Tux64BootExecStartPtr   start; 
-   register Tux64UInt32          param_memory_total   __asm__("$s0");
-   register Tux64BootLoadStatus  param_load_status    __asm__("$s1");
-   register Tux64UInt32          stack_pointer        __asm__("$sp");
+   const void * entrypoint;
 
-   start.data = (const void *)TUX64_LITERAL_UINTPTR(TUX64_PLATFORM_MIPS_N64_MEMORY_MAP_ADDRESS_RSP_IMEM);
+   entrypoint = (const void *)TUX64_LITERAL_UINTPTR(TUX64_PLATFORM_MIPS_N64_MEMORY_MAP_ADDRESS_RSP_IMEM);
 
-   param_memory_total   = memory_total;
-   param_load_status    = load_status;
-   stack_pointer        = TUX64_LITERAL_UINT32(TUX64_BOOT_STAGE2_STACK_ADDRESS + TUX64_BOOT_STAGE2_STACK_BYTES);
-
-   TUX64_EXPLICIT_DEPENDENCY(param_memory_total);
-   TUX64_EXPLICIT_DEPENDENCY(param_load_status);
-   TUX64_EXPLICIT_DEPENDENCY(stack_pointer);
-   start.function();
+   /* same issue as above, but now we also have to set the stack pointer. */
+   __asm__ volatile (
+      ".set noreorder\n"
+      "lui $sp,%1\n"
+      "jr %0\n"
+      "addiu $sp,$sp,%2\n"
+      :: "r"      (entrypoint),
+         "K"      (TUX64_BOOT_EXEC_STAGE2_STACK_POINTER >> TUX64_LITERAL_UINT8(16u)),
+         "K"      (TUX64_BOOT_EXEC_STAGE2_STACK_POINTER & TUX64_LITERAL_UINT32(TUX64_UINT16_MAX)),
+         "{s0}"   (memory_total),
+         "{s1}"   (load_status)
+   );
    TUX64_UNREACHABLE;
 }
 
